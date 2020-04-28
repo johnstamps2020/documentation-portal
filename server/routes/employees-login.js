569d1308d928f4d62e877c77706ef50a2c77e241
Pawel Kowaluk <pkowaluk@guidewire.com>
2020-04-28 14:05:24 +0200

    implement separate configurations for dev and prod

--- server/routes/employees-login.js
+++ server/routes/employees-login.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 let gwUrl = new URL(`${process.env.OKTA_DOMAIN}/oauth2/v1/authorize`);
-gwUrl.searchParams.set('idp', '0oa25tk18zhGOqMfj357');
+gwUrl.searchParams.set('idp', process.env.OKTA_IDP);
 gwUrl.searchParams.set('client_id', process.env.OKTA_CLIENT_ID);
 gwUrl.searchParams.set('scope', 'openid profile');
 gwUrl.searchParams.set('response_type', 'code');
