1b1315b4e3dddbb8ca019a44586a25f8e8b25f97
Michal Skowron <mskowron@guidewire.com>
2020-04-29 09:02:53 +0200

    DOCS-854: Reformats code with prettier

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {ExpressOIDC} = require('@okta/oidc-middleware');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
 
 const loginGatewayRoute = '/gw-login';
 
@@ -7,30 +7,30 @@ const gwCommunityCustomerParam = 'guidewire-customer';
 const gwCommunityPartnerParam = 'guidewire-partner';
 
 const oktaOIDC = new ExpressOIDC({
-    issuer: `${process.env.OKTA_DOMAIN}`,
-    client_id: `${process.env.OKTA_CLIENT_ID}`,
-    client_secret: `${process.env.OKTA_CLIENT_SECRET}`,
-    appBaseUrl: `${process.env.APP_BASE_URL}`,
-    scope: 'openid profile',
+  issuer: `${process.env.OKTA_DOMAIN}`,
+  client_id: `${process.env.OKTA_CLIENT_ID}`,
+  client_secret: `${process.env.OKTA_CLIENT_SECRET}`,
+  appBaseUrl: `${process.env.APP_BASE_URL}`,
+  scope: 'openid profile',
 });
 
 const authGateway = (req, res, next) => {
-    if (req.isAuthenticated() || process.env.DEV === 'yes') {
-        if (req.session.redirectTo) {
-            const redirectTo = req.session.redirectTo;
-            delete req.session.redirectTo;
-            res.redirect(redirectTo);
-        } else {
-            next();
-        }
-    } else if (req.query.authSource === gwCommunityCustomerParam) {
-        res.redirect('/customers-login')
-    } else if (req.query.authSource === gwCommunityPartnerParam) {
-        res.redirect('/partners-login')
+  if (req.isAuthenticated() || process.env.DEV === 'yes') {
+    if (req.session.redirectTo) {
+      const redirectTo = req.session.redirectTo;
+      delete req.session.redirectTo;
+      res.redirect(redirectTo);
     } else {
-        req.session.redirectTo = req.path;
-        res.redirect(loginGatewayRoute);
+      next();
     }
+  } else if (req.query.authSource === gwCommunityCustomerParam) {
+    res.redirect('/customers-login');
+  } else if (req.query.authSource === gwCommunityPartnerParam) {
+    res.redirect('/partners-login');
+  } else {
+    req.session.redirectTo = req.path;
+    res.redirect(loginGatewayRoute);
+  }
 };
 
-module.exports = {oktaOIDC, authGateway, loginGatewayRoute};
+module.exports = { oktaOIDC, authGateway, loginGatewayRoute };
