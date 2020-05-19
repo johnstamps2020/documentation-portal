82615682dd4d0d70b6e52fb517adcc212ca7cf48
Michal Skowron <mskowron@guidewire.com>
2020-05-19 16:38:04 +0200

    DOCS-935: Adds a processor for html5 files

--- dita_processor/html5_processor.py
+++ dita_processor/html5_processor.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import shutil
 from pathlib import Path
+from string import Template
 
 from lxml import etree
 
@@ -15,11 +16,12 @@ html_parser = etree.HTMLParser()
 with open("assets/header.html") as header_file:
     header_element = etree.fromstring(header_file.read())
 
-with open("assets/head.html") as head_file:
+with open("assets/head.txt") as head_file:
     head_tags = head_file.readlines()
 
 with open("assets/footer.html") as footer_file:
-    footer_fragment = etree.fromstring(footer_file.read())
+    footer_element_template = Template(footer_file.read())
+    footer_element = etree.fromstring(footer_element_template.substitute(current_year=current_year))
 
 if out_dir.exists():
     shutil.rmtree(out_dir)
@@ -29,12 +31,12 @@ for file in out_dir.glob("**/*.html"):
     file_tree = etree.parse(str(file), parser=html_parser)
     body_element = file_tree.find("body")
     body_element.insert(0, header_element)
+    body_element.insert(len(body_element.getchildren()), footer_element)
 
     head_element = file_tree.find("head")
     for head_tag in head_tags:
-        head_element.insert(-1, etree.fromstring(head_tag))
+        head_element.insert(len(head_element.getchildren()), etree.fromstring(head_tag))
 
     with open(file, 'w') as target_file:
-        # file_tree.write(target_file, method="html", pretty_print=True, encoding="utf-8")
         file_tree_string = etree.tostring(file_tree, method="html", pretty_print=True).decode('utf-8')
         target_file.write(file_tree_string)
