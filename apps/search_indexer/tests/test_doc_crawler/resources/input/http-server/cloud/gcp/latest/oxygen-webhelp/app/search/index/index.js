f45d5dfd510ef21ff3ce48a36cf858bd9178525a
Michal Skowron <mskowron@guidewire.com>
2020-05-20 12:16:33 +0200

    DOCS-935: Restructures the directory with Python apps

---
+++ apps/search_indexer/tests/test_doc_crawler/resources/input/http-server/cloud/gcp/latest/oxygen-webhelp/app/search/index/index.js
@@ -0,0 +1,25 @@
+define(["test_elastic_search/resources/input/http-server/cloud/gcp/latest/oxygen-webhelp/app/search/index/stopwords", "tests/test_doc_crawler/resources/input/http-server/cloud/gcp/latest/oxygen-webhelp/app/search/index/index-1", "index-2", "index-3", "htmlFileInfoList", "link2parent", "jquery"], function(stopwords, index1, index2, index3, fileInfoList, link2parent, $) {
+
+    var words = $.extend({}, index1, index2, index3);
+
+    return {
+        /**
+         * The object with indexed words.
+         *
+         * {"word" : "topicID*score, topicID*score"}
+         */
+        w : words,
+        /**
+         * Auto generated list of analyzer stop words that must be ignored by search.
+         */
+        stopWords : stopwords,
+
+        /**
+         * File info list.
+         */
+        fil : fileInfoList,
+
+        link2parent : link2parent
+
+    };
+});
