ec2cf8250f74d347439d1489d05ec8456da2a12b
Pawel Kowaluk <pkowaluk@guidewire.com>
2020-05-29 16:05:37 +0200

    DOCS-936: Ugly styled toc

--- server/public/scripts/dita.js
+++ server/public/scripts/dita.js
@@ -1,19 +1,59 @@
 function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
+  // see if DOM is already available
+  if (
+    document.readyState === 'complete' ||
+    document.readyState === 'interactive'
+  ) {
+    // call on next available tick
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function expandTree(startingElement) {
+  startingElement.classList.toggle('expanded');
+  const childList = startingElement.querySelector('ul');
+  if (childList) {
+    childList.classList.toggle('expanded');
+  }
+  let startingElementsParent = startingElement.parentElement;
+  while (startingElementsParent && startingElementsParent.tagName !== 'nav') {
+    startingElementsParent.classList.toggle('expanded');
+    startingElementsParent = startingElementsParent.parentElement;
+  }
 }
 
 docReady(function() {
-    const addressEnd = window.location.href.substring(window.location.href.lastIndexOf('/') + 1)
-    console.log('ADDRESS', addressEnd);
+  const addressEnd = window.location.href.substring(
+    window.location.href.lastIndexOf('/') + 1
+  );
 
-    const linkToThisPage = document.querySelector(`li a[href$="${addressEnd}"`);
-    if (linkToThisPage) {
-        linkToThisPage.classList.toggle('selected');
-    }
+  const linkToThisPage = document.querySelector(`li a[href$="${addressEnd}"`);
+  if (linkToThisPage) {
+    linkToThisPage.classList.toggle('selectedNavItem');
+  }
+
+  const nestedLists = document.querySelectorAll('li > ul');
+  nestedLists.forEach(list => {
+    list.classList.toggle('nested');
+    const parentLi = list.parentElement;
+    parentLi.classList.toggle('caret');
+  });
+
+  const activeLi = document.querySelector('nav[role="toc"] .selectedNavItem');
+  if (activeLi) {
+    expandTree(activeLi.parentElement);
+  }
+
+  const nonLinkingNavElements = document.querySelectorAll(
+    'nav[role="toc"] span'
+  );
+  nonLinkingNavElements.forEach(span => {
+    span.addEventListener('click', function() {
+      console.log('You clicked me', this);
+      const clickedLi = this.parentElement;
+      expandTree(clickedLi);
+    });
+  });
 });
