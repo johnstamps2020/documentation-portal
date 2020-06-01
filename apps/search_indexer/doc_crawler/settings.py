ef0a634e122d0bb90fb33b0325579ba4d4a05f87
Michal Skowron <mskowron@guidewire.com>
2020-06-01 09:37:23 +0200

    DOCS-968: Reduce console logging
    - Redirect log messages from the indexer to a file
    - Expose the log file as an artifact in the TeamCity build
    - Add a rule to gitignore to ignore the .idea/libraries directory
    - Update paths to venvs in the project settings

--- apps/search_indexer/doc_crawler/settings.py
+++ apps/search_indexer/doc_crawler/settings.py
@@ -1,6 +1,7 @@
 BOT_NAME = 'doc_crawler'
 SPIDER_MODULES = ['doc_crawler.spiders']
 LOG_LEVEL = 'INFO'
+LOG_FILE = 'search_indexer.log'
 DEFAULT_REQUEST_HEADERS = {'Referer': 'https://docs.guidewire.com'}
 ITEM_PIPELINES = {
     'doc_crawler.pipelines.BrokenLinkPipeline': 100,
