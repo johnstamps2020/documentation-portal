3d646a98dda7b841d2f0670bbfc84049274d2e2e
Pawel Kowaluk <pkowaluk@guidewire.com>
2020-07-16 16:18:29 +0200

    DOCS-1098: add a script that cleans up the index

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -952,6 +952,37 @@ object LoadSearchIndex : BuildType({
     }
 })
 
+object CleanUpIndex : BuildType({
+    name = "Clean up indes"
+    description = "Remove documents from index which are not in the config"
+
+    params {
+        select("env.DEPLOY_ENV", "", label = "Deployment environment", description = "Select an environment on which you want clean up the index", display = ParameterDisplay.PROMPT,
+                options = listOf("dev", "int", "staging", "prod"))
+    }
+
+    steps {
+        script {
+            name = "Run the cleanup script"
+            scriptContent = """
+                #!/bin/bash
+                set -xe
+                if [[ "%env.DEPLOY_ENV%" == "int" ]]; then
+                    export CONFIG_FILE=%teamcity.build.checkoutDir%/.teamcity/config/gw-docs-int.json
+                else
+                    export CONFIG_FILE=%teamcity.build.checkoutDir%/.teamcity/config/gw-docs-staging.json
+                fi
+                
+                pip install elasticsearch
+                cd apps/index_cleaner
+                python main.py ${'$'}{CONFIG_FILE}
+            """.trimIndent()
+            dockerImage = "python:3.8-slim"
+            dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
+        }
+    }
+})
+
 object TestContent : BuildType({
     name = "Test"
 
@@ -1458,6 +1489,7 @@ object Content : Project({
     builds.forEach(this::buildType)
 
     buildType(LoadSearchIndex)
+    buildType(CleanUpIndex)
     subProject(DeployServices)
     buildType(TestContent)
     subProject(Helpers.getContentProjectFromConfig("dev", "config/gw-docs-staging.json"))
