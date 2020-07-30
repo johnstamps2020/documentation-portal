cc61843c8d095a51a3e84bd7eeca6a89043455d7
Pawel Kowaluk <pkowaluk@guidewire.com>
2020-07-30 16:59:22 +0200

    remove unnecessary logging

--- server/routes/cloud-home.js
+++ server/routes/cloud-home.js
@@ -1,7 +1,6 @@
 const cloudProductList = require('../controllers/cloudProductController');
 
 const cloudHome = (req, res, next) => {
-  console.log('Address in BROWSER!', req.originalUrl);
   if (req.originalUrl === '/') {
     res.render('cloud-home', { products: cloudProductList });
   } else {
