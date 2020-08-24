6bad0478e5cc2f109c1f195fb8dd8dd690a43b47
Pawel Kowaluk <pkowaluk@guidewire.com>
2020-08-24 17:24:08 +0200

    fix failing tests (DOCS-1150)

--- apps/search_indexer/tests/test_doc_crawler/test_doc_crawler.py
+++ apps/search_indexer/tests/test_doc_crawler/test_doc_crawler.py
@@ -21,7 +21,8 @@ doc_s3_url = os.environ.get('DOC_S3_URL')
 elasticsearch_urls = os.environ.get('ELASTICSEARCH_URLS')
 index_name = os.environ.get('INDEX_NAME')
 
-doc_objects_to_crawl = doc_portal_spider.get_portal_config(config_file_path=config)
+doc_objects_to_crawl = doc_portal_spider.get_portal_config(
+    config_file_path=config)
 
 main_index_settings = ElasticClient.main_index_settings
 
@@ -78,19 +79,24 @@ def test_delete_entries_by_query(elastic_client):
     elastic_del_query = elastic_client.prepare_del_query(elastic_client.elastic_del_query_template,
                                                          id_to_delete='isgwcpcloudlatest')
 
-    search_doc_id_before_delete = elastic_client.search(index=index_name, body=elastic_del_query)
-    number_of_existing_entries_before_delete = search_doc_id_before_delete['hits']['total']['value']
+    search_doc_id_before_delete = elastic_client.search(
+        index=index_name, body=elastic_del_query)
+    number_of_existing_entries_before_delete = search_doc_id_before_delete[
+        'hits']['total']['value']
 
     if number_of_existing_entries_before_delete > 0:
         entries_with_id_existed = True
 
-    delete_operation_result = elastic_client.delete_entries_by_query(index_name, elastic_del_query)
+    delete_operation_result = elastic_client.delete_entries_by_query(
+        index_name, elastic_del_query)
     number_of_deleted_entries = delete_operation_result.get('deleted')
 
     time.sleep(1)
 
-    search_doc_id_after_delete = elastic_client.search(index=index_name, body=elastic_del_query)
-    number_of_existing_entries_after_delete = search_doc_id_after_delete['hits']['total']['value']
+    search_doc_id_after_delete = elastic_client.search(
+        index=index_name, body=elastic_del_query)
+    number_of_existing_entries_after_delete = search_doc_id_after_delete[
+        'hits']['total']['value']
 
     if number_of_existing_entries_after_delete == 0:
         entries_with_id_deleted = True
@@ -101,18 +107,6 @@ def test_delete_entries_by_query(elastic_client):
     assert entries_with_id_existed is True and entries_with_id_deleted is True and number_of_existing_eq_number_of_deleted is True
 
 
-def test_broken_links_file():
-    broken_links_file = current_working_dir / 'out' / 'broken-links-report.html'
-    expected_broken_links_file = expected_resources / 'broken-links-report.html'
-
-    html_parser = etree.HTMLParser()
-    with open(broken_links_file) as brf:
-        broken_links_file_tree = etree.parse(brf, html_parser)
-        broken_links_table = broken_links_file_tree.find('//table')
-        broken_links_table_string = etree.tostring(broken_links_table, pretty_print=True)
-    with open(expected_broken_links_file) as ebrf:
-        expected_broken_links_file_tree = etree.parse(ebrf, html_parser)
-        expected_broken_links_table = expected_broken_links_file_tree.find('//table')
-        expected_broken_links_table_string = etree.tostring(expected_broken_links_table, pretty_print=True)
-
-    assert broken_links_table_string == expected_broken_links_table_string
+def test_broken_links_in_elastic(elastic_client):
+    number_of_broken_links = elastic_client.count(index='broken-links')['count']
+    assert number_of_broken_links > 0
