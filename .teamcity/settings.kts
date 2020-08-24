4268dc7d77c9dc670337374f617b0600fd62585b
Michal Skowron <mskowron@guidewire.com>
2020-08-24 12:39:09 +0200

    DOCS-1144: Remove an unused import

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -9,7 +9,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.v2019_2.ui.insert
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import org.json.JSONArray
 import org.json.JSONObject
