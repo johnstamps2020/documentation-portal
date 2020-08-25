655933d59909b9ec4339400eff9c9971ec16a6f1
Pawel Kowaluk <pkowaluk@guidewire.com>
2020-08-25 10:57:34 +0200

    simplify crawler code (DOCS-1150)

--- apps/search_indexer/doc_crawler/pipelines.py
+++ apps/search_indexer/doc_crawler/pipelines.py
@@ -9,57 +9,10 @@ from .items import IndexEntry
 from .report_generator import report_generator
 
 
-class BrokenLinkPipeline:
-    index_name = 'broken-links'
-    elastic_client = None
-    number_of_created_entries = 0
-    failed_entries = []
-
-    def open_spider(self, spider):
-        search_app_urls = os.environ.get('ELASTICSEARCH_URLS', None).split(' ')
-        """ We turned off certificate validation in the Elasticsearch client because we don't need it.
-        So when you connect to an https link, a warning is issued that your request is insecure.
-        We turned off the warning as well.
-        """
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        self.elastic_client = ElasticClient(search_app_urls, use_ssl=False, verify_certs=False,
-                                            ssl_show_warn=False)
-
-        self.elastic_client.create_index(
-            self.index_name, self.elastic_client.main_index_settings)
-
-        for doc in spider.docs:
-            id_to_delete = doc["id"]
-            elastic_del_query = self.elastic_client.prepare_del_query(self.elastic_client.elastic_del_query_template,
-                                                                      id_to_delete=id_to_delete)
-            self.elastic_client.delete_entries_by_query(
-                self.index_name, elastic_del_query)
-
-    def close_spider(self, spider):
-        self.elastic_client.logger_instance.info(
-            f'\nCreated entries/Failures: {self.number_of_created_entries}/{len(self.failed_entries)}')
-        if self.failed_entries:
-            self.elastic_client.logger_instance.info(
-                f'Failed to load the following entries:')
-            for failed_entry in self.failed_entries:
-                self.elastic_client.logger_instance.info(f'\t{failed_entry}')
-
-    def process_item(self, item, spider):
-        if item.__class__.__name__ == BrokenLink.__name__:
-            broken_link_item = dict(item)
-            create_operation_result = self.elastic_client.index(
-                index=self.index_name, body=broken_link_item)
-            if create_operation_result.get('result') == 'created':
-                self.number_of_created_entries += 1
-            elif create_operation_result.get('result') != 'created':
-                self.failed_entries.append(broken_link_item)
-
-        return item
-
-
 class ElasticsearchPipeline:
     elastic_client = None
     index_name = ''
+    index_name_broken_links = 'broken-links'
     number_of_created_entries = 0
     failed_entries = []
 
@@ -77,6 +30,8 @@ class ElasticsearchPipeline:
 
         self.elastic_client.create_index(
             self.index_name, self.elastic_client.main_index_settings)
+        self.elastic_client.create_index(
+            self.index_name_broken_links, self.elastic_client.main_index_settings)
 
         for doc in spider.docs:
             id_to_delete = doc["id"]
@@ -84,6 +39,8 @@ class ElasticsearchPipeline:
                                                                       id_to_delete=id_to_delete)
             self.elastic_client.delete_entries_by_query(
                 self.index_name, elastic_del_query)
+            self.elastic_client.delete_entries_by_query(
+                self.index_name_broken_links, elastic_del_query)
 
     def close_spider(self, spider):
         self.elastic_client.logger_instance.info(
@@ -96,12 +53,20 @@ class ElasticsearchPipeline:
 
     def process_item(self, item, spider):
         if item.__class__.__name__ == IndexEntry.__name__:
-            index_entry = dict(item)
-            create_operation_result = self.elastic_client.index(
-                index=self.index_name, body=index_entry)
-            if create_operation_result.get('result') == 'created':
-                self.number_of_created_entries += 1
-            elif create_operation_result.get('result') != 'created':
-                self.failed_entries.append(index_entry)
+            index_name = self.index_name
+        elif item.__class__.__name__ == BrokenLink.__name__:
+            index_name = self.index_name_broken_links
+        else:
+            self.elastic_client.logger_instance.warning(
+                f'Item not added to Elasticsearch. Reason: Item of unknown type. Item: {item}, class name: {item.__class__.__name__}')
+            return item
+
+        index_entry = dict(item)
+        create_operation_result = self.elastic_client.index(
+            index=index_name, body=index_entry)
+        if create_operation_result.get('result') == 'created':
+            self.number_of_created_entries += 1
+        elif create_operation_result.get('result') != 'created':
+            self.failed_entries.append(index_entry)
 
         return item
