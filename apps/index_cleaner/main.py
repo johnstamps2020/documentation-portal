63d6a8cdfbc91a9d07e2aff2fc72f45a727aeec1
Pawel Kowaluk <pkowaluk@guidewire.com>
2020-08-27 09:24:52 +0200

    add broken-links to the list of indexes to clean (DOCS-1155)

--- apps/index_cleaner/main.py
+++ apps/index_cleaner/main.py
@@ -20,35 +20,40 @@ for doc in config_json['docs']:
     if (doc_id and doc_id not in ids_in_config):
         ids_in_config.append(doc_id)
 
-index_name = 'gw-docs'
 
-if client.indices.exists(index=index_name):
-    search_body = {
-        "size": 42,
-        "query": {
-            "match_all": {}
+def clean_index(index_name):
+    print(f'Cleaning the {index_name} index...')
+    if client.indices.exists(index=index_name):
+        search_body = {
+            "size": 42,
+            "query": {
+                "match_all": {}
+            }
         }
-    }
-    all_ids = []
+        all_ids = []
 
-    resp = helpers.scan(client, index=index_name)
-    for doc in list(resp):
-        doc_id = doc['_source'].get('doc_id', None)
-        if (doc_id and doc_id not in all_ids):
-            all_ids.append(doc_id)
+        resp = helpers.scan(client, index=index_name)
+        for doc in list(resp):
+            doc_id = doc['_source'].get('doc_id', None)
+            if (doc_id and doc_id not in all_ids):
+                all_ids.append(doc_id)
 
-    count = 0
-    for id_in_index in all_ids:
-        if id_in_index not in ids_in_config:
-            print(f'Deleting docs with id "{id_in_index}"')
-            client.delete_by_query(index=index_name, body={
-                "query": {
-                    "match": {
-                        "doc_id": {
-                            "query": id_in_index
-                        }
-                    },
-                }
-            })
-            count += 1
-    print(f'Deleted {count} ids')
+        count = 0
+        for id_in_index in all_ids:
+            if id_in_index not in ids_in_config:
+                print(f'Deleting docs with id "{id_in_index}"')
+                client.delete_by_query(index=index_name, body={
+                    "query": {
+                        "match": {
+                            "doc_id": {
+                                "query": id_in_index
+                            }
+                        },
+                    }
+                })
+                count += 1
+        print(f'Deleted {count} ids from {index_name}')
+
+
+clean_index('gw-docs')
+clean_index('broken-links')
