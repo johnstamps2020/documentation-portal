a808b7cda57d1f8633dbdfa5b37585aa23f87881
Miles Gilster <mgilster@guidewire.com>
2020-09-18 12:24:41 -0700

    adjust product for docs by productFamily

--- server/controllers/cloudProductFamilyController.js
+++ server/controllers/cloudProductFamilyController.js
@@ -11,18 +11,24 @@ async function getCloudProductFamilies() {
         for (const doc of config.docs) {
           if (doc.metadata.products && doc.metadata.platform && doc.metadata.platform.includes('Cloud')) {
             if (doc.metadata.products.some(r => productFamily.products.includes(r))) {
+              // copy doc values to new object and remove products from clone that are not in productFamily
+              let docToAdd = JSON.parse(JSON.stringify(doc));
+              let filteredProducts = [];
+              filteredProducts = docToAdd.metadata.products.filter(product => productFamily.products.includes(product));              
+              docToAdd.metadata.products = filteredProducts;
+
               const existingFamily = cloudProductFamilies.find(
                 f => f.name === productFamily.productFamilyName
               );
               if (existingFamily) {
                 existingFamily.href = `products/${existingFamily.id}`;
-                existingFamily.docs.push(doc);
+                existingFamily.docs.push(docToAdd);
               } else {
                 cloudProductFamilies.push({
                   id: productFamily.productFamilyName.toLowerCase().replace(/\W/g, '-'),
                   name: productFamily.productFamilyName,
-                  href: doc.url,
-                  docs: [doc],
+                  href: docToAdd.url,
+                  docs: [docToAdd],
                 });
               }
             }
