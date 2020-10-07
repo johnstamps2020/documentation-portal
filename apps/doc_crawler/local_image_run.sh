969a1192edd8bdf9688da7dd889a106f4d560491
Pawel Kowaluk <pkowaluk@guidewire.com>
2020-10-07 11:20:50 +0200

    Make app pick up the right config in Dockerfile

--- apps/doc_crawler/local_image_run.sh
+++ apps/doc_crawler/local_image_run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -xe
 
-# ./build_docker.sh
-
 docker run -i \
 -v /Users/pkowaluk/Documents/git-repos/documentation-portal/.teamcity/config:/config \
 --env-file ./.env-file \
-doc-crawler doc_crawler
+-w /opt/app \
+doc-crawler
