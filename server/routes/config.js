f1f0e2d4b06186760eeec665095edab053b94e91
mskowron <mskowron@guidewire.com>
2020-12-07 11:49:27 +0100

    DOCS-1294: Get docs from config endpoints (improved)

--- server/routes/config.js
+++ server/routes/config.js
@@ -5,10 +5,45 @@ const { getDocs } = require('../controllers/configController');
 
 router.get('/', async function(req, res, next) {
   try {
+    res.send('Try /config/docs');
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/docs', async function(req, res, next) {
+  try {
     const docs = await getDocs(
       {
         query: {
           match_all: {},
+        },
+      },
+      configIndexName
+    );
+    res.send(docs);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/docs/findMany', async function(req, res, next) {
+  try {
+    const { product, platform, version } = req.query;
+    const queryFields = [
+      { match_phrase: { 'metadata.platform': platform } },
+      { match_phrase: { 'metadata.product': product } },
+    ];
+    if (version) {
+      queryFields.push({ match_phrase: { 'metadata.version': version } });
+    }
+    const docs = await getDocs(
+      {
+        query: {
+          bool: {
+            must: queryFields,
+            must_not: [{ match: { displayOnLandingPages: 'false' } }],
+          },
         },
       },
       configIndexName
