bb7516947d237940c6d1c7f37313bb2208fdd801
Pawel Kowaluk <pkowaluk@guidewire.com>
2021-01-22 11:51:36 +0100

    Use Python 3.8.3 from DockerHub (DOCS-1418)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2292,7 +2292,7 @@ object BuildSphinx : Template({
                 make html
                 cp -r ./_build/html ./build
             """.trimIndent()
-            dockerImage = "artifactory.guidewire.com/doctools-docker-dev/python:3.8-slim-buster"
+            dockerImage = "python:3.8.3"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
             dockerPull = true
         }
