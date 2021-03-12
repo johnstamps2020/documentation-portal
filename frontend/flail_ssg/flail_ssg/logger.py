5dfc57779d73b310966b13936c820e67fd2830a0
Miles Gilster <mgilster@guidewire.com>
2021-03-12 10:23:52 -0800

    update Flail logger to use utf-8 encoding

--- frontend/flail_ssg/flail_ssg/logger.py
+++ frontend/flail_ssg/flail_ssg/logger.py
@@ -4,13 +4,15 @@ from pathlib import Path
 
 def configure_logger(name: str, logging_level: str, log_path: Path):
     logger_instance = logging.getLogger(name)
-    weights = {'critical': 50, 'error': 40, 'warning': 30, 'info': 20, 'debug': 10}
+    weights = {'critical': 50, 'error': 40,
+               'warning': 30, 'info': 20, 'debug': 10}
     try:
         logger_instance.setLevel(weights[logging_level.lower()])
         log_console_handler = logging.StreamHandler()
         log_console_handler.setLevel(weights[logging_level.lower()])
         logger_instance.addHandler(log_console_handler)
-        log_file_handler = logging.FileHandler(log_path, mode='w')
+        log_file_handler = logging.FileHandler(
+            log_path, mode='w', encoding='utf-8')
         log_file_handler.setLevel(weights[logging_level.lower()])
         logger_instance.addHandler(log_file_handler)
         for h in logger_instance.handlers:
