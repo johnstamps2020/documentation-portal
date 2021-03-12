70bfa7bd2fcd7016dda289a72c2c831176214b32
Pawel Kowaluk <pkowaluk@guidewire.com>
2021-03-12 10:24:57 +0100

    back after they log out too (JUT-6938)

--- server/routes/gw-logout.js
+++ server/routes/gw-logout.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
+  const cameFrom = req.headers.referer || '/';
   req.session.destroy(function(err) {
     if (err) {
       next(err);
     }
-    res.redirect('/');
+    res.redirect(cameFrom);
   });
 });
 
