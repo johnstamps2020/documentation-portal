ba799a86b51d696e0c8754d27f7c6d630d4c129b
mskowron <mskowron@guidewire.com>
2021-03-17 16:07:46 +0100

    DOCS-1497 - Remove taxonomy configs
    - Remove taxonomy config files
    - Remove deployment of taxonomy files from the config deployer
    - Update the config deployer tests

--- apps/config_deployer/config_deployer/main.py
+++ apps/config_deployer/config_deployer/main.py
@@ -1,9 +1,9 @@
 import json
+import logging
 import os
 import shutil
 from pathlib import Path
-from typing import Dict, List
-import logging
+from typing import Dict
 
 
 def get_docs_for_env(config_file: Path, env_name: str) -> Dict:
@@ -12,28 +12,6 @@ def get_docs_for_env(config_file: Path, env_name: str) -> Dict:
         json_data.pop('$schema')
         json_data['docs'] = [doc for doc in json_data['docs'] if env_name in doc['environments']]
     return json_data
-
-
-def get_releases_from_docs(json_data: Dict):
-    release_lists = [doc.get('metadata').get('release') for doc in json_data['docs']
-                     if doc.get('metadata').get('release') and doc.get('displayOnLandingPages') is not False]
-
-    releases = []
-    for single_release_list in release_lists:
-        for release in single_release_list:
-            releases.append(release)
-    unique_releases = sorted(set(releases))
-    return unique_releases
-
-
-def get_paths_index(dir_to_index: Path, allowed_items: List = None):
-    paths_in_dir = [path for path in dir_to_index.iterdir()]
-    if allowed_items:
-        paths_in_dir = [path for path in dir_to_index.iterdir() if any(
-            allowed_item in path.stem for allowed_item in allowed_items)]
-    return {
-        'paths': sorted([path.name for path in paths_in_dir])
-    }
 
 
 def save_json_file(save_path: Path, config_content: Dict):
@@ -64,27 +42,6 @@ def main():
     config_file_out_path = out_dir / 'config.json'
     save_json_file(config_file_out_path, docs_for_env)
     logger.info(f'Server config file saved to {config_file_out_path}')
-
-    releases = get_releases_from_docs(docs_for_env)
-    cloud_taxonomies_path = config_path.parent / 'taxonomy' / 'cloud'
-
-    cloud_taxonomies_out_path = out_dir / 'taxonomy' / 'cloud'
-    if cloud_taxonomies_out_path.exists():
-        shutil.rmtree(cloud_taxonomies_out_path)
-    cloud_taxonomies_out_path.mkdir(exist_ok=True, parents=True)
-    cloud_taxonomy_files_paths = get_paths_index(cloud_taxonomies_path, releases)
-    cloud_taxonomy_index_out_path = cloud_taxonomies_out_path / 'index.json'
-    save_json_file(cloud_taxonomy_index_out_path, cloud_taxonomy_files_paths)
-    logger.info(f'Index for cloud taxonomy files saved to {cloud_taxonomy_index_out_path}')
-    for taxonomy_file_path in cloud_taxonomy_files_paths['paths']:
-        full_taxonomy_file_path = cloud_taxonomies_path / taxonomy_file_path
-        shutil.copy2(full_taxonomy_file_path, cloud_taxonomies_out_path)
-        logger.info(f'{taxonomy_file_path} copied to {cloud_taxonomies_out_path}')
-
-    self_managed_taxonomy_path = config_path.parent / 'taxonomy' / 'self-managed.json'
-    self_managed_taxonomy_out_path = out_dir / 'taxonomy'
-    shutil.copy2(self_managed_taxonomy_path, self_managed_taxonomy_out_path)
-    logger.info(f'{self_managed_taxonomy_path.name} copied to {self_managed_taxonomy_out_path}')
     logger.info('DONE!')
 
 
--- apps/config_deployer/tests/test_config_deployer.py
+++ apps/config_deployer/tests/test_config_deployer.py
@@ -31,26 +31,6 @@ def generate_server_configs():
         )
 
 
-@pytest.fixture(scope='session', autouse=True)
-def create_cloud_taxonomies_index():
-    full_cloud_taxonomies_out_path = out_resources_path / cloud_taxonomies_path
-    if full_cloud_taxonomies_out_path.exists():
-        shutil.rmtree(full_cloud_taxonomies_out_path)
-    full_cloud_taxonomies_out_path.mkdir(exist_ok=True, parents=True)
-    deploy_envs = ['dev', 'int', 'staging', 'prod']
-    for deploy_env in deploy_envs:
-        docs_for_env = config_deployer.get_docs_for_env(input_resources_path / server_config_path, deploy_env)
-        releases = config_deployer.get_releases_from_docs(docs_for_env)
-        cloud_taxonomy_files_paths = config_deployer.get_paths_index(
-            input_resources_path / cloud_taxonomies_path,
-            releases
-        )
-        config_deployer.save_json_file(
-            full_cloud_taxonomies_out_path / f'index_{deploy_env}.json',
-            cloud_taxonomy_files_paths
-        )
-
-
 @pytest.fixture(scope='session')
 def match_out_and_expected_files(request):
     matching_files_json_data = []
@@ -70,7 +50,6 @@ def match_out_and_expected_files(request):
     return matching_files_json_data
 
 
-# Test server configs
 def test_out_dir_has_expected_server_configs():
     dir_comp = filecmp.dircmp(out_resources_path / server_configs_out_path,
                               expected_resources_path / server_configs_out_path)
@@ -90,25 +69,3 @@ def test_docs_element(match_out_and_expected_files):
 
     test_number_of_docs_in_files()
     test_all_docs_are_included_in_files()
-
-
-# Test cloud taxonomy index files
-def test_out_dir_has_expected_taxonomy_indices():
-    dir_comp = filecmp.dircmp(out_resources_path / cloud_taxonomies_path,
-                              expected_resources_path / cloud_taxonomies_path)
-    assert dir_comp.left_list == dir_comp.right_list
-
-
-@pytest.mark.parametrize("match_out_and_expected_files", [cloud_taxonomies_path], indirect=True)
-def test_paths_element(match_out_and_expected_files):
-    def test_number_of_paths_in_files():
-        for json_data_pair in match_out_and_expected_files:
-            assert len(json_data_pair[0]['paths']) == len(json_data_pair[1]['paths'])
-
-    def test_all_releases_are_included_in_files():
-        for json_data_pair in match_out_and_expected_files:
-            for out_index_file in json_data_pair[0]['paths']:
-                assert any(out_index_file == exp_index_file for exp_index_file in json_data_pair[1]['paths'])
-
-    test_number_of_paths_in_files()
-    test_all_releases_are_included_in_files()
