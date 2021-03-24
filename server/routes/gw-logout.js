6a285a1a76b12a6fd17446de91a3e19e5c89495e
Pawel Kowaluk <pkowaluk@guidewire.com>
2021-03-24 11:42:23 +0100

    always redirect to the root page after logout

--- server/routes/gw-logout.js
+++ server/routes/gw-logout.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  const cameFrom = req.headers.referer || '/';
   req.session.destroy(function(err) {
     if (err) {
       next(err);
     }
-    res.redirect(cameFrom);
+    res.redirect('/');
   });
 });
 
