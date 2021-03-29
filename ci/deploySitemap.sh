2b59e43ac510b8dd455f9674fd70d5a61b1899ed
pkowaluk <pkowaluk@guidewire.com>
2021-03-29 12:46:54 +0200

    Use the default docportal app (DOCS-1480)

--- ci/deploySitemap.sh
+++ ci/deploySitemap.sh
@@ -3,5 +3,5 @@ set -eux
 
 aws eks update-kubeconfig --name atmos-${DEPLOY_ENV}
 
-export POD_NAME=$(kubectl get pods -l app=docportal-flail-app --namespace=${NAMESPACE} | cut -d' ' -f1 | tail -n +2)
+export POD_NAME=$(kubectl get pods -l app=docportal-app --namespace=${NAMESPACE} | cut -d' ' -f1 | tail -n +2)
 kubectl cp ${OUTPUT_FILE} ${POD_NAME}:/usr/src/app/public/sitemap.xml --namespace=${NAMESPACE}
