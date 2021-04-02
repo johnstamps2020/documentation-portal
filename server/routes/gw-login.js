2e99d43cda39eba519725bd83e16da2153d5bf47
mskowron <mskowron@guidewire.com>
2021-04-02 12:45:31 +0200

    DOCS-1518 - Remove the logic for rewriting the req.session.redirectTo

--- server/routes/gw-login.js
+++ server/routes/gw-login.js
@@ -2,17 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  const cameFrom = req.headers.referer;
-  if (
-    cameFrom &&
-    cameFrom.startsWith(process.env.APP_BASE_URL) &&
-    !cameFrom.endsWith('gw-login') &&
-    !cameFrom.endsWith('gw-login/')
-  ) {
-    req.session.redirectTo = cameFrom;
-  } else {
-    req.session.redirectTo = '/';
-  }
   res.render('gw-login');
 });
 
