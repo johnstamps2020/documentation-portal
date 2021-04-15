85dfc563581bdade0dbd02a8a681190944b7cb6a
mskowron <mskowron@guidewire.com>
2021-04-15 11:23:41 +0200

    DOCS-1498 - Fix the app name in the deployment script

--- ci/deployFrontend.sh
+++ ci/deployFrontend.sh
@@ -3,7 +3,6 @@ set -eux
 
 aws eks update-kubeconfig --name atmos-${DEPLOY_ENV}
 
-#TODO: Update the app name to docportal-app before mergin to master
-export POD_NAME=$(kubectl get pods -l app=docportal-flail-app --namespace=${NAMESPACE} | cut -d' ' -f1 | tail -n +2)
+export POD_NAME=$(kubectl get pods -l app=docportal-app --namespace=${NAMESPACE} | cut -d' ' -f1 | tail -n +2)
 kubectl cp ${OUTPUT_DIR} ${POD_NAME}:/tmp/out --namespace=${NAMESPACE}
 kubectl exec ${POD_NAME} --namespace=${NAMESPACE} -- sh -c 'rm -rf /usr/src/app/pages/* && cp -R /tmp/out/* /usr/src/app/pages/ && rm -rf /tmp/out'
