da12638cd33665bdecb7c5fb1f81bb348bc378ed
mskowron <mskowron@guidewire.com>
2021-04-16 12:08:15 +0200

    DOCS-1512 - Fix dir creation for static files

--- ci/deployFrontend.sh
+++ ci/deployFrontend.sh
@@ -5,4 +5,4 @@ aws eks update-kubeconfig --name atmos-${DEPLOY_ENV}
 
 export POD_NAME=$(kubectl get pods -l app=docportal-app --namespace=${NAMESPACE} | cut -d' ' -f1 | tail -n +2)
 kubectl cp ${OUTPUT_DIR} ${POD_NAME}:/tmp/out --namespace=${NAMESPACE}
-kubectl exec ${POD_NAME} --namespace=${NAMESPACE} -- sh -c 'rm -rf /usr/src/app/static/pages/* && cp -R /tmp/out/* /usr/src/app/static/pages/ && rm -rf /tmp/out'
+kubectl exec ${POD_NAME} --namespace=${NAMESPACE} -- sh -c 'rm -rf /usr/src/app/static/pages && mkdir /usr/src/app/static/pages && cp -R /tmp/out/* /usr/src/app/static/pages/ && rm -rf /tmp/out'
