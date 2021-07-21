7866754e46ad44a69d6b66d44bee2c2e70810abc
mskowron <mskowron@guidewire.com>
2021-07-21 14:41:17 +0200

    DOCS-1616 - Add an admin panel CLI for managing config files

--- apps/config_deployer/config_deployer/processor.py
+++ apps/config_deployer/config_deployer/processor.py
@@ -1,3 +1,7 @@
+# TODO: Support all the operations for sources and builds:
+# - Add support for properties that are not metadata (e.g. 'metadata.product' and then split the name)
+# TODO: Add a CLI (argparse)
+
 import json
 from pathlib import Path
 
@@ -113,43 +117,43 @@ def clone_objects_with_updated_metadata_property(src_file: Path, root_object_nam
 # Testing part
 
 # merge
-# merge_input = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/merge/input')
-#
-# merge_output = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/merge/output')
-#
-# all_docs = merge_objects(merge_input, 'docs')
-# docs_obj = add_schema_reference({
-#     'docs': all_docs
-# })
-# save_json_file(merge_output / '_merge-docs.json', docs_obj)
+merge_input = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/merge/input')
+
+merge_output = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/merge/output')
+
+all_docs = merge_objects(merge_input, 'docs')
+docs_obj = add_schema_reference({
+    'docs': all_docs
+})
+save_json_file(merge_output / '_merge-docs.json', docs_obj)
 
 # split
-# split_input = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/split/input/docs.json')
-# split_output = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/split/output')
-#
-# chunked_docs = split_objects_into_chunks(split_input, 'docs', 10)
-# for chunk_number, chunk in enumerate(chunked_docs):
-#     docs_obj = add_schema_reference(
-#         {
-#             'docs': chunk
-#         }
-#     )
-#     save_json_file(split_output / f'_split-docs-chunk-{chunk_number}.json', docs_obj)
-#
-# docs_by_product = split_objects_by_metadata_property(split_input, 'docs', 'product')
-# for version_docs_obj in docs_by_product:
-#     docs_obj = add_schema_reference(
-#         {
-#             'docs': version_docs_obj['docs']
-#         }
-#     )
-#     save_json_file(
-#         split_output / f'_split-docs-{version_docs_obj["property_name"].casefold()}-{version_docs_obj["property_value"].casefold()}.json',
-#         docs_obj)
+split_input = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/split/input/docs.json')
+split_output = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/split/output')
+
+chunked_docs = split_objects_into_chunks(split_input, 'docs', 10)
+for chunk_number, chunk in enumerate(chunked_docs):
+    docs_obj = add_schema_reference(
+        {
+            'docs': chunk
+        }
+    )
+    save_json_file(split_output / f'_split-docs-chunk-{chunk_number}.json', docs_obj)
+
+docs_by_product = split_objects_by_metadata_property(split_input, 'docs', 'product')
+for version_docs_obj in docs_by_product:
+    docs_obj = add_schema_reference(
+        {
+            'docs': version_docs_obj['docs']
+        }
+    )
+    save_json_file(
+        split_output / f'_split-docs-{version_docs_obj["property_name"].casefold()}-{version_docs_obj["property_value"].casefold()}.json',
+        docs_obj)
 
 # docs_by_version = split_objects_by_metadata_property(split_input, 'docs', 'version')
 # for version_docs_obj in docs_by_version:
@@ -161,57 +165,57 @@ def clone_objects_with_updated_metadata_property(src_file: Path, root_object_nam
 #     save_json_file(split_output / f'_split-docs-{version_docs_obj["property_value"].casefold()}.json', docs_obj)
 
 # remove
-# remove_input = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/remove/input/docs.json')
-# remove_output = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/remove/output')
-#
-# docs_without_product_bc = remove_objects_by_metadata_property(remove_input, 'docs', 'product', 'BillingCenter')
-# no_bc_product_docs_obj = add_schema_reference({
-#     'docs': docs_without_product_bc
-# })
-# save_json_file(remove_output / '_remove-docs-product-billingcenter.json', no_bc_product_docs_obj)
-#
-# docs_without_version_1011 = remove_objects_by_metadata_property(remove_input, 'docs', 'version', '10.1.1')
-# no_version_1011_docs_obj = add_schema_reference({
-#     'docs': docs_without_version_1011
-# })
-# save_json_file(remove_output / '_remove-docs-version-10.1.1.json', no_version_1011_docs_obj)
-#
-# # extract
-# extract_input = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/extract/input/docs.json')
-# extract_output = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/extract/output')
-#
-# updated_docs, extracted_docs = extract_objects_by_metadata_property(extract_input, 'docs', 'product', 'BillingCenter')
-# updated_docs_obj = add_schema_reference({
-#     'docs': updated_docs
-# })
-# extracted_docs_obj = add_schema_reference({
-#     'docs': extracted_docs
-# })
-# save_json_file(extract_output / '_extract-docs-removed-product-billingcenter.json', updated_docs_obj)
-# save_json_file(extract_output / '_extract-docs-product-billingcenter.json', extracted_docs_obj)
+remove_input = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/remove/input/docs.json')
+remove_output = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/remove/output')
+
+docs_without_product_bc = remove_objects_by_metadata_property(remove_input, 'docs', 'product', 'BillingCenter')
+no_bc_product_docs_obj = add_schema_reference({
+    'docs': docs_without_product_bc
+})
+save_json_file(remove_output / '_remove-docs-product-billingcenter.json', no_bc_product_docs_obj)
+
+docs_without_version_1011 = remove_objects_by_metadata_property(remove_input, 'docs', 'version', '10.1.1')
+no_version_1011_docs_obj = add_schema_reference({
+    'docs': docs_without_version_1011
+})
+save_json_file(remove_output / '_remove-docs-version-10.1.1.json', no_version_1011_docs_obj)
+
+# extract
+extract_input = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/extract/input/docs.json')
+extract_output = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/extract/output')
+
+updated_docs, extracted_docs = extract_objects_by_metadata_property(extract_input, 'docs', 'product', 'BillingCenter')
+updated_docs_obj = add_schema_reference({
+    'docs': updated_docs
+})
+extracted_docs_obj = add_schema_reference({
+    'docs': extracted_docs
+})
+save_json_file(extract_output / '_extract-docs-removed-product-billingcenter.json', updated_docs_obj)
+save_json_file(extract_output / '_extract-docs-product-billingcenter.json', extracted_docs_obj)
 
 # update
-# update_input = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/update/input/docs.json')
-# update_output = Path(
-#     '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/update/output')
-#
-# docs_with_updated_version = update_metadata_property_for_objects(update_input, 'docs', 'version', '10.1.2', '10.1.3')
-# updated_version_docs_obj = add_schema_reference({
-#     'docs': docs_with_updated_version
-# })
-# save_json_file(update_output / '_update-docs-version-10.1.2-to-10.1.3.json', updated_version_docs_obj)
-#
-# docs_with_updated_product = update_metadata_property_for_objects(update_input, 'docs', 'product', 'ClaimCenter',
-#                                                                  'XCenter')
-# updated_product_docs_obj = add_schema_reference({
-#     'docs': docs_with_updated_product
-# })
-# save_json_file(update_output / '_update-docs-product-claimcenter-to-xcenter.json', updated_product_docs_obj)
+update_input = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/update/input/docs.json')
+update_output = Path(
+    '/Users/mskowron/Documents/GIT-REPOS/documentation-portal/apps/config_deployer/tests/admin_panel_cli/update/output')
+
+docs_with_updated_version = update_metadata_property_for_objects(update_input, 'docs', 'version', '10.1.2', '10.1.3')
+updated_version_docs_obj = add_schema_reference({
+    'docs': docs_with_updated_version
+})
+save_json_file(update_output / '_update-docs-version-10.1.2-to-10.1.3.json', updated_version_docs_obj)
+
+docs_with_updated_product = update_metadata_property_for_objects(update_input, 'docs', 'product', 'ClaimCenter',
+                                                                 'XCenter')
+updated_product_docs_obj = add_schema_reference({
+    'docs': docs_with_updated_product
+})
+save_json_file(update_output / '_update-docs-product-claimcenter-to-xcenter.json', updated_product_docs_obj)
 
 # clone
 clone_input = Path(
