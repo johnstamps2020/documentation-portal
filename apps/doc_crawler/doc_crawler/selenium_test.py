e9760487762994aab6e164e3b54136c57803b3ad
mskowron <mskowron@guidewire.com>
2021-08-05 15:37:53 +0200

    Revert "DOCS-1240 - (WIP) Testing Selenium for Python"
    This reverts commit 4d59f59452da7e362207ad0e324ae492048882ce.

--- apps/doc_crawler/doc_crawler/selenium_test.py
+++ apps/doc_crawler/doc_crawler/selenium_test.py
@@ -1,42 +1,27 @@
 from selenium import webdriver
-from selenium.common import exceptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
-# driver.get(
-#     'https://portal2.internal.us-east-2.service.guidewire.net/portal/secure/doc/bc/BillingCenter_3.0.4/doc')
-# try:
-#     WebDriverWait(driver, 15).until(
-#         EC.presence_of_element_located((By.TAG_NAME, 'frameset'))
-#     )
-#     frames = driver.find_elements_by_tag_name('frame')
-#     for f in frames:
-#         print(f.get_attribute('src'))
-#     driver.implicitly_wait(10)
-#     driver.switch_to.frame(1)
-#     subframes = driver.find_elements_by_tag_name('frame')
-#     for sf in subframes:
-#         print(sf.get_attribute('src'))
-#     driver.implicitly_wait(10)
-#     driver.switch_to.frame(1)
-#     links = driver.find_elements_by_tag_name('a')
-#     for l in links:
-#         print(l.get_attribute('href'))
-# finally:
-#     driver.quit()
-
 driver.get(
-    'https://ditaot.internal.us-east-2.service.guidewire.net/jutro/storybook/5.3.1/')
+    'https://portal2.internal.us-east-2.service.guidewire.net/portal/secure/doc/bc/BillingCenter_3.0.4/doc')
 try:
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.XPATH, '//div[@id="root"]'))
+    element = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.TAG_NAME, 'frameset'))
     )
+    frames = driver.find_elements_by_tag_name('frame')
+    for f in frames:
+        print(f.get_attribute('src'))
+    driver.implicitly_wait(10)
+    driver.switch_to.frame(1)
+    subframes = driver.find_elements_by_tag_name('frame')
+    for sf in subframes:
+        print(sf.get_attribute('src'))
+    driver.implicitly_wait(10)
+    driver.switch_to.frame(1)
     links = driver.find_elements_by_tag_name('a')
-    for link in links:
-        print(link.get_attribute('href'))
-except exceptions.StaleElementReferenceException as e:
-    print(e)
+    for l in links:
+        print(l.get_attribute('href'))
 finally:
     driver.quit()
