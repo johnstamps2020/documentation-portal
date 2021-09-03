d677e9002782456d1d9d301894807525c406fb49
mskowron <mskowron@guidewire.com>
2021-09-03 10:02:59 +0200

    DOCS-1931 - Change the composite build to normal build

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1892,7 +1892,6 @@ object HelperObjects {
         class BuildApiListenerBuild(vcs_root_id: RelativeId, source_id: String) : BuildType({
             name = "$source_id listener"
             id = RelativeId(vcs_root_id.toString() + "_query")
-            type = Type.COMPOSITE
             vcs {
                 root(vcs_root_id)
                 cleanCheckout = true
@@ -3049,7 +3048,7 @@ object CreateReleaseTag : BuildType({
 })
 
 object BuildApiBuildRunner : BuildType({
-    name = "Build API Build Runner"
+    name = "Build API ruild runner"
     maxRunningBuilds = 1
 
     params {
