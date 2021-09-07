46a64c603a4eddb68baf18e1e9f0e841198c4d5e
Miles Gilster <mgilster@guidewire.com>
2021-09-07 13:55:07 -0700

    Add upgradediffs-page-builder

---
+++ apps/upgradediffs_page_builder/publish_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -xe
+./build_docker.sh
+
+echo "STREAMING INFORMATION FOR HUMAN OPERATORS"
+
+IMAGE_VERSION=$1
+
+rx='^([0-9]+\.){0,2}(\*|[0-9]+)$'
+if [[ $IMAGE_VERSION =~ $rx || $IMAGE_VERSION = 'latest' ]]; then
+ echo "INFO: Version $IMAGE_VERSION"
+else
+ echo "MESSAGE: Version unavailable or invalid: '$IMAGE_VERSION'. Applying 'develop' instead"
+ IMAGE_VERSION=develop
+ echo "HUMAN VERIFICATION AUTHORIZED. PRINTING A DEBUG STATEMENT"
+ echo "Version set to: $IMAGE_VERSION"
+fi
+
+docker login -u $ARTIFACTORY_USERNAME --password $ARTIFACTORY_PASSWORD artifactory.guidewire.com
+
+docker tag upgradediffs-page-builder artifactory.guidewire.com/doctools-docker-dev/upgradediffs-page-builder:$IMAGE_VERSION
+docker push artifactory.guidewire.com/doctools-docker-dev/upgradediffs-page-builder:$IMAGE_VERSION
+
+echo "END TRANSMISSION"
