c3bc35680aa8497e75555257a67307cb517e6182
mskowron <mskowron@guidewire.com>
2021-09-16 09:20:20 +0200

    DOCS-1931 - Move config parameters to a dataclass

--- apps/build_manager/build_manager/main.py
+++ apps/build_manager/build_manager/main.py
@@ -1,4 +1,5 @@
 import copy
+import dataclasses
 import json
 import logging
 import os
@@ -14,6 +15,47 @@ logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 
 @dataclass
+class AppConfig:
+    build_api_url: str = os.environ.get('BUILD_API_URL')
+    admin_server_api_key: str = os.environ.get('ADMIN_SERVER_API_KEY')
+    git_url: str = os.environ.get('GIT_URL')
+    git_build_branch: str = os.environ.get('GIT_BUILD_BRANCH')
+    teamcity_build_queue_url = os.environ.get('TEAMCITY_BUILD_QUEUE_URL')
+    _teamcity_api_auth_token = os.environ.get('TEAMCITY_API_AUTH_TOKEN')
+    _changed_files_file = os.environ.get('CHANGED_FILES_FILE')
+    build_api_headers = {
+        'Content-Type': 'application/json',
+        'X-API-Key': admin_server_api_key
+    }
+    teamcity_api_headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {_teamcity_api_auth_token}'
+    }
+
+    @property
+    def changed_files_file(self):
+        return Path(self._changed_files_file)
+
+    def get_build_api_data(self, resources):
+        return {
+            'gitUrl': self.git_url,
+            'gitBranch': self.git_build_branch,
+            'resources': resources
+        }
+
+    def get_app_config(self):
+        missing_parameters = [
+            field.name.upper().lstrip('_') for field in dataclasses.fields(self)
+            if not getattr(self, field.name)
+        ]
+        if missing_parameters:
+            raise SystemError(f'Missing environment variables:'
+                              f'\n{", ".join(missing_parameters)}')
+        return self
+
+
+@dataclass
 class BuildInfo:
     id: str
     href: str
@@ -23,37 +65,19 @@ class BuildInfo:
     estimated_time_to_finish: int
 
 
-def get_changed_files() -> list[str]:
-    changed_files_file = Path(os.environ['CHANGED_FILES_FILE'])
-    return [line.split(':')[0] for line in changed_files_file.open().readlines()]
+def get_changed_files(app_config: AppConfig) -> list[str]:
+    return [line.split(':')[0] for line in app_config.changed_files_file.open().readlines()]
 
 
-def get_build_ids(resources: list[str]) -> list[str]:
-    build_api_url = os.environ['BUILD_API_URL']
-    admin_server_api_key = os.environ['ADMIN_SERVER_API_KEY']
-    git_url = os.environ['GIT_URL']
-    git_build_branch = os.environ['GIT_BUILD_BRANCH']
-    data = {
-        'gitUrl': git_url,
-        'gitBranch': git_build_branch,
-        'resources': resources
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'X-API-Key': admin_server_api_key
-    }
-    response = requests.get(build_api_url, headers=headers, json=data)
+def get_build_ids(app_config: AppConfig, resources: list[str]) -> list[str]:
+    response = requests.get(
+        app_config.build_api_url,
+        headers=app_config.build_api_headers,
+        json=app_config.get_build_api_data(resources))
     return sorted([build['build_id'] for build in response.json()])
 
 
-def start_builds(build_ids: list[str]) -> list[BuildInfo]:
-    teamcity_build_queue_url = os.environ['TEAMCITY_BUILD_QUEUE_URL']
-    teamcity_api_auth_token = os.environ['TEAMCITY_API_AUTH_TOKEN']
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': f'Bearer {teamcity_api_auth_token}'
-    }
+def start_builds(app_config: AppConfig, build_ids: list[str]) -> list[BuildInfo]:
     started_builds = []
     for build_id in build_ids:
         data = {
@@ -61,7 +85,10 @@ def start_builds(build_ids: list[str]) -> list[BuildInfo]:
                 'id': build_id
             }
         }
-        response = requests.post(teamcity_build_queue_url, headers=headers, data=json.dumps(data))
+        response = requests.post(
+            app_config.teamcity_build_queue_url,
+            headers=app_config.teamcity_api_headers,
+            data=json.dumps(data))
         response_json = response.json()
         started_builds.append(BuildInfo(
             id=response_json['id'],
@@ -75,18 +102,11 @@ def start_builds(build_ids: list[str]) -> list[BuildInfo]:
     return started_builds
 
 
-def update_builds_info(builds_to_check: list[BuildInfo]) -> list[BuildInfo]:
-    teamcity_build_queue_url = os.environ['TEAMCITY_BUILD_QUEUE_URL']
-    teamcity_api_auth_token = os.environ['TEAMCITY_API_AUTH_TOKEN']
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': f'Bearer {teamcity_api_auth_token}'
-    }
+def update_builds_info(app_config: AppConfig, builds_to_check: list[BuildInfo]) -> list[BuildInfo]:
     checked_builds = []
     for build in builds_to_check:
-        full_build_href = urllib.parse.urljoin(teamcity_build_queue_url, build.href)
-        response = requests.get(full_build_href, headers=headers)
+        full_build_href = urllib.parse.urljoin(app_config.teamcity_build_queue_url, build.href)
+        response = requests.get(full_build_href, headers=app_config.teamcity_api_headers)
         build_info = response.json()
         build.state = build_info['state']
         build.status = build_info.get('status', '')
@@ -101,7 +121,7 @@ def update_builds_info(builds_to_check: list[BuildInfo]) -> list[BuildInfo]:
     return checked_builds
 
 
-def coordinate_builds(waiting_builds: list[str], triggered_builds: list[BuildInfo]):
+def coordinate_builds(app_config: AppConfig, waiting_builds: list[str], triggered_builds: list[BuildInfo]):
     queued_builds = [build for build in triggered_builds if build.state.casefold() == 'queued']
     running_builds = [build for build in triggered_builds if build.state.casefold() == 'running']
     finished_builds = [build for build in triggered_builds if build.state.casefold() == 'finished']
@@ -138,7 +158,7 @@ def coordinate_builds(waiting_builds: list[str], triggered_builds: list[BuildInf
             updated_waiting_builds.remove(build)
             number_of_empty_slots -= 1
     if planned_builds:
-        started_builds = start_builds(planned_builds)
+        started_builds = start_builds(app_config, planned_builds)
         logging.info(f'Number of newly started builds: {len(started_builds)}')
         triggered_builds += started_builds
 
@@ -154,14 +174,14 @@ def coordinate_builds(waiting_builds: list[str], triggered_builds: list[BuildInf
     time.sleep(wait_time)
 
     logging.info('Checking the status of all triggered builds...')
-    updated_triggered_builds = update_builds_info(triggered_builds)
+    updated_triggered_builds = update_builds_info(app_config, triggered_builds)
     updated_active_builds = [
         build
         for build in updated_triggered_builds
         if build.state.casefold() in ['queued', 'running']
     ]
     if updated_waiting_builds or updated_active_builds:
-        coordinate_builds(updated_waiting_builds, updated_triggered_builds)
+        coordinate_builds(app_config, updated_waiting_builds, updated_triggered_builds)
     else:
         logging.info('All builds finished')
         unsuccessful_builds = [build for build in updated_triggered_builds if
@@ -179,11 +199,12 @@ def coordinate_builds(waiting_builds: list[str], triggered_builds: list[BuildInf
 
 
 def main():
-    changed_files = get_changed_files()
+    build_manager_config = AppConfig().get_app_config()
+    changed_files = get_changed_files(build_manager_config)
     if changed_files:
-        builds_to_start = get_build_ids(changed_files)
+        builds_to_start = get_build_ids(build_manager_config, changed_files)
         if builds_to_start:
-            coordinate_builds(builds_to_start, [])
+            coordinate_builds(build_manager_config, builds_to_start, [])
         else:
             logging.info('No build IDs found for the detected changes. Nothing more to do here.')
             sys.exit(0)
