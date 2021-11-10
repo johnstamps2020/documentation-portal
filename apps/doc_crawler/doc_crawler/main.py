6059e3cb7cb39814114bb4de818e9168c8f055a1
mskowron <mskowron@guidewire.com>
2021-11-10 10:29:17 +0100

    DOCS-2057 - Use CrawlerProcess to prevent build hanging
    - Use one queue for all processes
    - Handle errors from deferred objects and exit with a proper code

--- apps/doc_crawler/doc_crawler/main.py
+++ apps/doc_crawler/doc_crawler/main.py
@@ -13,24 +13,28 @@ def create_crawler_process(spider, docs_to_crawl: List, root_url: str, s3_bucket
     try:
         crawler_settings = get_project_settings()
         crawler_settings.set('LOG_FILE', f'doc_crawler_{docs_to_crawl[0]["id"]}.log')
-        process = CrawlerProcess(crawler_settings)
-        process.crawl(spider, docs=docs_to_crawl, app_base_url=root_url,
-                      doc_s3_url=s3_bucket_url)
-        process.start()
+        crawler_process = CrawlerProcess(crawler_settings)
+        deferred = crawler_process.crawl(spider, docs=docs_to_crawl, app_base_url=root_url,
+                                         doc_s3_url=s3_bucket_url)
+        crawler_process.start()
+        if deferred.result:
+            raise deferred.result.value
         q.put(None)
     except Exception as e:
         q.put(e)
 
 
-def run_spider(spider, docs_to_crawl: List, root_url: str, s3_bucket_url: str):
+def run_spiders(spider, docs_to_crawl: List, root_url: str, s3_bucket_url: str):
+    all_results = []
     queue = Queue()
-    process = Process(target=create_crawler_process, args=(spider, docs_to_crawl, root_url, s3_bucket_url, queue,))
-    process.start()
-    result = queue.get()
-    process.join()
+    for doc in docs_to_crawl:
+        process = Process(target=create_crawler_process, args=(spider, [doc], root_url, s3_bucket_url, queue,))
+        process.start()
+        all_results.append(queue.get())
+        process.join()
 
-    if result is not None:
-        raise result
+    if [r for r in all_results if r is not None]:
+        raise Exception('Crawling process finished with errors')
 
 
 def env_is_set():
@@ -66,74 +70,9 @@ def main():
                          '\n\t- The config file does not contain the provided doc ID.'
                          '\n\t- The provided doc ID is invalid.')
 
-    # Testing
-    doc_objects_to_crawl = [
-        {
-            "id": "isbc202111dataarchiving",
-            "title": "Data Archiving",
-            "url": "cloud/bc/202111/dataarchiving",
-            "metadata": {
-                "platform": [
-                    "Cloud"
-                ],
-                "product": [
-                    "BillingCenter"
-                ],
-                "version": [
-                    "2021.11"
-                ],
-                "release": [
-                    "Dobson"
-                ],
-                "subject": [
-                    "Administration"
-                ]
-            },
-            "environments": [
-                "dev",
-                "int",
-                "staging"
-            ],
-            "displayOnLandingPages": True,
-            "indexForSearch": True,
-            "public": False
-        },
-        {
-            "id": "isbc202111devsetup",
-            "title": "Developer Setup",
-            "url": "cloud/bc/202111/devsetup",
-            "metadata": {
-                "platform": [
-                    "Cloud"
-                ],
-                "product": [
-                    "BillingCenter"
-                ],
-                "version": [
-                    "2021.11"
-                ],
-                "release": [
-                    "Dobson"
-                ],
-                "subject": [
-                    "Installation"
-                ]
-            },
-            "environments": [
-                "dev",
-                "int",
-                "staging"
-            ],
-            "displayOnLandingPages": True,
-            "indexForSearch": True,
-            "public": False
-        },
-    ]
-    # Testing
-    for doc_object_to_crawl in doc_objects_to_crawl:
-        run_spider(spider=doc_portal_spider.DocPortalSpider, docs_to_crawl=[doc_object_to_crawl],
-                   root_url=app_base_url,
-                   s3_bucket_url=doc_s3_url)
+    run_spiders(spider=doc_portal_spider.DocPortalSpider, docs_to_crawl=doc_objects_to_crawl,
+                root_url=app_base_url,
+                s3_bucket_url=doc_s3_url)
 
 
 if __name__ == '__main__':
