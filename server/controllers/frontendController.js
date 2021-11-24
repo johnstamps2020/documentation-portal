f6312e20146303add35c6b6e101a98714e1c0ba4
mskowron <mskowron@guidewire.com>
2021-11-24 13:22:58 +0100

    Render templates on server

---
+++ server/controllers/frontendController.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const staticPagesDir = path.join(__dirname, '..', 'static', 'pages');
+
+async function getPage(req, res, next) {
+  const configFilePath = path.join(staticPagesDir, req.path, 'index.json');
+  const configFileExists = fs.existsSync(configFilePath);
+  if (configFileExists) {
+    const fileContents = fs.readFileSync(configFilePath, 'utf-8');
+    const fileContentsJson = JSON.parse(fileContents);
+    const templateName = fileContentsJson.template;
+    res.render(templateName, fileContentsJson);
+  } else {
+    next();
+  }
+}
+
+module.exports = { getPage };
