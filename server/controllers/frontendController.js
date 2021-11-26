2c836d34c07ab1f3edd7726d8e62a232ac86237f
mskowron <mskowron@guidewire.com>
2021-11-26 10:09:13 +0100

    DOCS-2090 - Fix localization info

--- server/controllers/frontendController.js
+++ server/controllers/frontendController.js
@@ -3,58 +3,58 @@ const path = require('path');
 const staticPagesDir = path.join(__dirname, '..', 'static', 'pages');
 
 function setL10nParams(pageClass) {
-  let ns = {};
-  ns.placeholder = 'Search';
-  ns.lang = 'en';
+  let l10n = {};
+  l10n.placeholder = 'Search';
+  l10n.lang = 'en';
   if (pageClass) {
     if (pageClass.includes('l10n')) {
       {
         if (pageClass.includes(' de-DE ')) {
           {
-            ns.placeholder = 'Suche';
-            ns.lang = 'de';
+            l10n.placeholder = 'Suche';
+            l10n.lang = 'de';
           }
-          if (pageClass.includes(' fr-FR ')) {
-            {
-              ns.placeholder = 'Chercher';
-              ns.lang = 'fr';
-            }
-            if (pageClass.includes(' es-ES ')) {
-              {
-                ns.placeholder = 'Buscar';
-                ns.lang = 'es';
-              }
-              if (pageClass.includes(' es-LA ')) {
-                ns.placeholder = 'Buscar';
-                ns.lang = 'es';
-              }
-              if (pageClass.includes(' it-IT ')) {
-                ns.placeholder = 'Ricerca';
-                ns.lang = 'it';
-              }
-              if (pageClass.includes(' ja-JP ')) {
-                ns.placeholder = '探す';
-                ns.lang = 'ja';
-              }
-              if (pageClass.includes(' nl-NL ')) {
-                ns.placeholder = 'Zoeken';
-                ns.lang = 'nl';
-              }
-              if (pageClass.includes(' pt-BR ')) {
-                ns.placeholder = 'Procurar';
-                ns.lang = 'pt';
-              }
-              if (pageClass.includes(' ru ')) {
-                ns.placeholder = 'Поиск';
-                ns.lang = 'ru';
-              }
-            }
+        }
+        if (pageClass.includes(' fr-FR ')) {
+          {
+            l10n.placeholder = 'Chercher';
+            l10n.lang = 'fr';
           }
+        }
+        if (pageClass.includes(' es-ES ')) {
+          {
+            l10n.placeholder = 'Buscar';
+            l10n.lang = 'es';
+          }
+        }
+        if (pageClass.includes(' es-LA ')) {
+          l10n.placeholder = 'Buscar';
+          l10n.lang = 'es';
+        }
+        if (pageClass.includes(' it-IT ')) {
+          l10n.placeholder = 'Ricerca';
+          l10n.lang = 'it';
+        }
+        if (pageClass.includes(' ja-JP ')) {
+          l10n.placeholder = '探す';
+          l10n.lang = 'ja';
+        }
+        if (pageClass.includes(' nl-NL ')) {
+          l10n.placeholder = 'Zoeken';
+          l10n.lang = 'nl';
+        }
+        if (pageClass.includes(' pt-BR ')) {
+          l10n.placeholder = 'Procurar';
+          l10n.lang = 'pt';
+        }
+        if (pageClass.includes(' ru ')) {
+          l10n.placeholder = 'Поиск';
+          l10n.lang = 'ru';
         }
       }
     }
   }
-  return ns;
+  return l10n;
 }
 
 async function getPage(req, res, next) {
@@ -65,9 +65,9 @@ async function getPage(req, res, next) {
     const fileContentsJson = JSON.parse(fileContents);
     const templateName = fileContentsJson.template;
     res.render(templateName, {
-      pageConfig: fileContentsJson,
+      pageContent: fileContentsJson,
       pagePath: req.path,
-      ns: setL10nParams(fileContentsJson.class),
+      localizationInfo: setL10nParams(fileContentsJson.class),
     });
   } else {
     next();
