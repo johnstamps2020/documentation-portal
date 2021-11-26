2836b82e6c43c21feeb13d8f1472d5cdae8ad74d
mskowron <mskowron@guidewire.com>
2021-11-26 06:11:39 +0100

    DOCS-2090 - Set lang and other L10N params in the controller instead of the view

--- server/controllers/frontendController.js
+++ server/controllers/frontendController.js
@@ -2,6 +2,61 @@ const fs = require('fs');
 const path = require('path');
 const staticPagesDir = path.join(__dirname, '..', 'static', 'pages');
 
+function setL10nParams(pageClass) {
+  let ns = {};
+  ns.placeholder = 'Search';
+  ns.lang = 'en';
+  if (pageClass) {
+    if (pageClass.includes('l10n')) {
+      {
+        if (pageClass.includes(' de-DE ')) {
+          {
+            ns.placeholder = 'Suche';
+            ns.lang = 'de';
+          }
+          if (pageClass.includes(' fr-FR ')) {
+            {
+              ns.placeholder = 'Chercher';
+              ns.lang = 'fr';
+            }
+            if (pageClass.includes(' es-ES ')) {
+              {
+                ns.placeholder = 'Buscar';
+                ns.lang = 'es';
+              }
+              if (pageClass.includes(' es-LA ')) {
+                ns.placeholder = 'Buscar';
+                ns.lang = 'es';
+              }
+              if (pageClass.includes(' it-IT ')) {
+                ns.placeholder = 'Ricerca';
+                ns.lang = 'it';
+              }
+              if (pageClass.includes(' ja-JP ')) {
+                ns.placeholder = '探す';
+                ns.lang = 'ja';
+              }
+              if (pageClass.includes(' nl-NL ')) {
+                ns.placeholder = 'Zoeken';
+                ns.lang = 'nl';
+              }
+              if (pageClass.includes(' pt-BR ')) {
+                ns.placeholder = 'Procurar';
+                ns.lang = 'pt';
+              }
+              if (pageClass.includes(' ru ')) {
+                ns.placeholder = 'Поиск';
+                ns.lang = 'ru';
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return ns;
+}
+
 async function getPage(req, res, next) {
   const configFilePath = path.join(staticPagesDir, req.path, 'index.json');
   const configFileExists = fs.existsSync(configFilePath);
@@ -12,6 +67,7 @@ async function getPage(req, res, next) {
     res.render(templateName, {
       pageConfig: fileContentsJson,
       pagePath: req.path,
+      ns: setL10nParams(fileContentsJson.class),
     });
   } else {
     next();
