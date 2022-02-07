c33fa61ef5fe8d459bf322491a4b51b7aaeb0ef4
pkowaluk <pkowaluk@guidewire.com>
2022-02-07 14:40:35 +0100

    add the "skip nav" link

---
+++ server/public/scripts/modules/skipNav.js
@@ -0,0 +1,16 @@
+export function addSkipNav() {
+  let mainId = 'mainContent';
+  const main = document.querySelector('main');
+  if (main.id) {
+    mainId = main.id;
+  } else {
+    main.setAttribute('id', mainId);
+  }
+
+  const skipNav = document.createElement('a');
+  skipNav.setAttribute('href', `#${mainId}`);
+  skipNav.classList.add('skipNav');
+  skipNav.innerText = 'Skip to main content';
+
+  document.querySelector('body').prepend(skipNav);
+}
