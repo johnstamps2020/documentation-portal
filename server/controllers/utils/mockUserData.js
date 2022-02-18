e8c57c402f27fcaf30787d9e5ee086773f5cbd5a
pkowaluk <pkowaluk@guidewire.com>
2022-02-18 13:43:24 +0100

    add mock user data for "external users"

---
+++ server/controllers/utils/mockUserData.js
@@ -0,0 +1,16 @@
+module.exports = {
+  internal: {
+    isLoggedIn: true,
+    hasGuidewireEmail: true,
+    name: 'Alfred Lord Tennyson',
+    preferred_username: 'atennyson@guidewire.com',
+    locale: 'en-US',
+  },
+  external: {
+    isLoggedIn: true,
+    hasGuidewireEmail: false,
+    name: 'Gerard Manley Hopkins',
+    preferred_username: 'ghopkins@gcustomer.com',
+    locale: 'en-US',
+  },
+};
