28938ff17d15af05eec12dfb0ea995f7c11fe18c
mskowron <mskowron@guidewire.com>
2022-03-07 13:55:48 +0100

    DOCS-2102 - Align internal page styles with the Jutro design

--- server/routes/internal.js
+++ server/routes/internal.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const { getUserInfo } = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('internal');
+  const userInfo = getUserInfo(req);
+  const pageInfo = {
+    userName: userInfo.name,
+    userEmail: userInfo.preferred_username,
+  };
+  res.render('internal', { pageInfo });
 });
 
 module.exports = router;
