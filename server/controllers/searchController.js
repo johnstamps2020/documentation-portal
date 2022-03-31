d8b11d3a682f15e156bf103a24809b6ac54e4b97
pkowaluk <pkowaluk@guidewire.com>
2022-03-31 17:00:03 +0200

    add styling to the error page (chore)

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -119,6 +119,7 @@ async function getFilters(query, fieldMappings, urlFilters) {
 }
 
 async function runSearch(query, startIndex, resultsPerPage, urlFilters) {
+  mch;
   const queryWithFiltersFromUrl = JSON.parse(JSON.stringify(query));
   if (urlFilters) {
     let queryFilters = queryWithFiltersFromUrl.bool.hasOwnProperty('filter')
