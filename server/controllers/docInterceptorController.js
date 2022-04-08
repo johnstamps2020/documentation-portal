b66a6b17ecb09eb1183027a5ed09daa17f44ebc9
pkowaluk <pkowaluk@guidewire.com>
2022-04-08 17:37:05 +0200

    interceptor intial setup

---
+++ server/controllers/docInterceptorController.js
@@ -0,0 +1,37 @@
+const tagManagerHead = `
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PLXG9H5');</script>
+<!-- End Google Tag Manager -->
+`;
+
+const tagManagerBody = `
+<!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLXG9H5" height="0" width="0" style="display: none; visibility: hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+`;
+
+function containsHtml(str) {
+  const match = str.match(/<html/i);
+  return match;
+}
+
+// function appendToHead(htmlString, textToAppend) {
+//     return str.
+// }
+
+async function interceptAndUpdateDocPage(responseBuffer) {
+  const response = responseBuffer.toString('utf8');
+  if (containsHtml(response)) {
+    console.log('Contains html');
+    return response.replace(/Single-Element/g, 'Sweat Drop');
+  } else {
+    console.log('non HTML response');
+    return responseBuffer;
+  }
+}
+
+module.exports = interceptAndUpdateDocPage;
