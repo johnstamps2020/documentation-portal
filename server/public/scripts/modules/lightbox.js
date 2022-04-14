c70df7935a5b2f8194af6341d5a3297c570c2972
mskowron <mskowron@guidewire.com>
2022-04-14 11:34:13 +0200

    DOCS-2398 - Add lightbox for tables

--- server/public/scripts/modules/lightbox.js
+++ server/public/scripts/modules/lightbox.js
@@ -1,23 +1,33 @@
 import React, { useRef } from 'react';
 import styles from '../../stylesheets/modules/lightbox.module.css';
 
-function Lightbox({ imgAttributes }) {
+function ThumbnailWithExternalButton({ thumbnail, showDialog }) {
+  return (
+    <>
+      <div>
+        <button
+          onClick={showDialog}
+          className={styles.expandButton}
+          title="Open full-size view"
+        />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: thumbnail }} />
+    </>
+  );
+}
+
+function ClickableThumbnail({ showDialog, thumbnail }) {
+  return (
+    <button
+      onClick={showDialog}
+      className={styles.lightboxButton}
+      dangerouslySetInnerHTML={{ __html: thumbnail }}
+    />
+  );
+}
+
+function Lightbox({ thumbnail, fullSizeElement, clickToEnlarge }) {
   const dialogRef = useRef();
-
-  const imgProps = [...imgAttributes].reduce((acc, attr) => {
-    acc[attr.name] =
-      attr.name === 'alt' ? `${attr.value}, click to enlarge` : attr.value;
-    return acc;
-  }, {});
-
-  const imgPropsForPreview = [...imgAttributes].reduce((acc, attr) => {
-    if (!['width', 'height'].includes(attr.name)) {
-      acc[attr.name] = attr.value;
-      return acc;
-    }
-
-    return acc;
-  }, {});
 
   function showDialog() {
     document.body.style.overflow = 'hidden';
@@ -31,37 +41,58 @@ function Lightbox({ imgAttributes }) {
 
   return (
     <>
-      <button onClick={showDialog} className={styles.lightboxButton}>
-        <img {...imgProps} />
-      </button>
+      {clickToEnlarge ? (
+        <ClickableThumbnail showDialog={showDialog} thumbnail={thumbnail} />
+      ) : (
+        <ThumbnailWithExternalButton
+          showDialog={showDialog}
+          thumbnail={thumbnail}
+        />
+      )}
       <dialog ref={dialogRef} className={styles.dialog}>
-        <div>
-          <img
-            {...imgPropsForPreview}
-            onClick={closeDialog}
-            className={styles.embiggenedImage}
-          />
-        </div>
+        <div
+          onClick={closeDialog}
+          className={
+            clickToEnlarge ? styles.embiggenedImage : styles.embiggenedTable
+          }
+          dangerouslySetInnerHTML={{ __html: fullSizeElement }}
+        />
         <button
           onClick={closeDialog}
           className={styles.closeButton}
-          title="Close image view"
-        ></button>
+          title="Close full-size view"
+        />
       </dialog>
     </>
   );
 }
 
 export async function addLightbox() {
-  const images = document.querySelectorAll('img');
-  if (images) {
+  const lightboxElements = document.querySelectorAll('img, table');
+  if (lightboxElements) {
     const { render } = await import('react-dom');
-    images.forEach((image, i) => {
+    lightboxElements.forEach((lightboxElem, i) => {
       const lightboxContainer = document.createElement('span');
       lightboxContainer.id = `lightbox${i}`;
-      image.before(lightboxContainer);
-      render(<Lightbox imgAttributes={image.attributes} />, lightboxContainer);
-      image.remove();
+      lightboxElem.before(lightboxContainer);
+      const thumbnail = lightboxElem.outerHTML;
+      const thumbnailDeepCopy = lightboxElem.cloneNode(true);
+      const isImage = lightboxElem.tagName.toLowerCase() === 'img';
+      if (isImage) {
+        ['width', 'height'].forEach(attrName =>
+          thumbnailDeepCopy.removeAttribute(attrName)
+        );
+      }
+      const fullSizeElement = thumbnailDeepCopy.outerHTML;
+      render(
+        <Lightbox
+          thumbnail={thumbnail}
+          fullSizeElement={fullSizeElement}
+          clickToEnlarge={isImage}
+        />,
+        lightboxContainer
+      );
+      lightboxElem.remove();
     });
   }
 }
