11772d104d57bab493b3e277dec62afd75edd776
Miles Gilster <mgilster@guidewire.com>
2022-04-14 15:00:03 -0700

    build.pdfs no for html5

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3680,7 +3680,7 @@ object GwBuildSteps {
                 commandParams.add(Pair("--git.branch", git_branch))
                 commandParams.add(Pair("-f", "html5-Guidewire"))
                 commandParams.add(Pair("--args.rellinks", "nofamily"))
-                commandParams.add(Pair("--build.pdfs", "yes"))
+                commandParams.add(Pair("--build.pdfs", "no"))
 
             }
         }
