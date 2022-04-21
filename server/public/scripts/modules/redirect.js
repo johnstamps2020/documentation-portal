b3cd73cc0ac66ccc4f642426e70ff03bcf4f5027
Miles Gilster <mgilster@guidewire.com>
2022-04-21 13:35:59 -0700

    better null check

--- server/public/scripts/modules/redirect.js
+++ server/public/scripts/modules/redirect.js
@@ -1,6 +1,6 @@
 export async function redirectToFirstTopic() {
     const firstLink = document.querySelector(".topicref > a");
-    if(!firstLink) {
+    if(firstLink === null) {
         console.error('Error: No first topic reference to redirect to.');
         return;
     }
