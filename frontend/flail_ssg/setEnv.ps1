8d9c86a4a99d3ad1f7dddf4034584dce0d7ba077
Miles Gilster <mgilster@guidewire.com>
2022-04-28 15:15:23 -0700

    DOCS-2430 add feature preview card to sidebar for Elysian

--- frontend/flail_ssg/setEnv.ps1
+++ frontend/flail_ssg/setEnv.ps1
@@ -1,7 +1,9 @@
-param ($envName = 'dev', $allowErrors = 'yes', $docPortalHome = 'C:\_git-repos\documentation-portal')
+param ($envName = 'int', $allowErrors = 'yes', $docPortalHome = 'C:\_git-repos\documentation-portal')
 $env:DEPLOY_ENV = $envName
 $env:PAGES_DIR = $docPortalHome + '\frontend\pages'
-$env:TEMPLATES_DIR = $docPortalHome + '\frontend\templates'
-$env:OUTPUT_DIR = $docPortalHome + '\server\public'
-$env:DOCS_CONFIG_FILE = $docPortalHome + '\.teamcity\config\docs\docs.json'
+$env:OUTPUT_DIR = $docPortalHome + '\server\static\pages'
+$env:OUTPUT_FORMAT = 'json'
+$env:DOCS_CONFIG_FILE = $docPortalHome + '\apps\config_deployer\out\merge-all.json'
 $env:SEND_BOUNCER_HOME = $allowErrors
+$env:PYTHONPATH = $docPortalHome
+$env:PAGE_SCHEMA_FILE = $docPortalHome + '\frontend\page-schema.json'
