bf47c95f4afba47cff4e534869050e8adab622b1
Miles Gilster <mgilster@guidewire.com>
2022-05-12 09:36:53 -0700

    disable interceptor for proxy issue, new try-catch in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,26 +11,33 @@ function belongsToGuidewire(email) {
 }
 
 function getUserInfo(req) {
-  if (process.env.ENABLE_AUTH === 'no') {
-    if (process.env.PRETEND_TO_BE_EXTERNAL === 'yes') {
-      return mockUserData.external;
+  try {
+    if (process.env.ENABLE_AUTH === 'no') {
+      if (process.env.PRETEND_TO_BE_EXTERNAL === 'yes') {
+        return mockUserData.external;
+      }
+      return mockUserData.internal;
     }
-    return mockUserData.internal;
-  }
 
-  const isLoggedIn = req.session.requestIsAuthenticated;
-  const userInfo = {
-    isLoggedIn: isLoggedIn,
-  };
-  const user = req.user;
-  if (user) {
-    const { name, preferred_username, locale } = user;
-    userInfo.hasGuidewireEmail = belongsToGuidewire(preferred_username);
-    userInfo.name = name;
-    userInfo.preferred_username = preferred_username;
-    userInfo.locale = locale;
+    const isLoggedIn = req.session.requestIsAuthenticated;
+    const userInfo = {
+      isLoggedIn: isLoggedIn,
+    };
+    const user = req.user;
+    if (user) {
+      const { name, preferred_username, locale } = user;
+      userInfo.hasGuidewireEmail = belongsToGuidewire(preferred_username);
+      userInfo.name = name;
+      userInfo.preferred_username = preferred_username;
+      userInfo.locale = locale;
+    }
+    return userInfo;
+  } catch (err) {
+        winstonLogger.error(
+          `Problem getting user info 
+              ERROR: ${err.message}`
+        );
   }
-  return userInfo;
 }
 
 module.exports = { getUserInfo };
