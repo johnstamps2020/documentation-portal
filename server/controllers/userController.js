56b2de9e2f563bf2c9f2e65ed7a6a822aa4d7770
Pawel Kowaluk <pkowaluk@guidewire.com>
2022-05-18 10:19:51 +0200

    return data even if user has no name (DOCS-2524)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,27 +14,41 @@ function belongsToGuidewire(email) {
   }
 }
 
+function getUserName(user) {
+  if (user?.name) {
+    return user.name;
+  }
+
+  if (user?.email) {
+    return user.email.split('@')[0];
+  }
+
+  return 'Authenticated User';
+}
+
 function getUserInfo(req) {
   try {
+    let userInfo = {};
+    let user = {};
     if (process.env.ENABLE_AUTH === 'no') {
       if (process.env.PRETEND_TO_BE_EXTERNAL === 'yes') {
-        return mockUserData.external;
+        user = { ...mockUserData.external };
+      } else {
+        user = { ...mockUserData.internal };
       }
-      return mockUserData.internal;
+
+      userInfo.isLoggedIn = true;
+    } else {
+      user = { ...req.user };
+      userInfo.isLoggedIn = req.session?.requestIsAuthenticated || false;
     }
 
-    const isLoggedIn = req.session.requestIsAuthenticated;
-    const userInfo = {
-      isLoggedIn: isLoggedIn,
-    };
-    const user = req.user;
-    if (user) {
-      const { name, locale, email } = user;
-      userInfo.hasGuidewireEmail = belongsToGuidewire(email);
-      userInfo.name = name;
-      userInfo.preferred_username = email.toLowerCase();
-      userInfo.locale = locale;
-    }
+    const { locale, email } = user;
+    userInfo.hasGuidewireEmail = belongsToGuidewire(email);
+    userInfo.name = getUserName(user);
+    userInfo.preferred_username = email.toLowerCase() || 'no email';
+    userInfo.locale = locale;
+
     return userInfo;
   } catch (err) {
     winstonLogger.error(
--- server/controllers/utils/mockUserData.js
+++ server/controllers/utils/mockUserData.js
@@ -1,16 +1,12 @@
 module.exports = {
   internal: {
-    isLoggedIn: true,
-    hasGuidewireEmail: true,
     name: 'Alfred Lord Tennyson',
-    preferred_username: 'atennyson@guidewire.com',
+    email: 'atennyson@guidewire.com',
     locale: 'en-US',
   },
   external: {
-    isLoggedIn: true,
-    hasGuidewireEmail: false,
     name: 'Gerard Manley Hopkins',
-    preferred_username: 'ghopkins@gcustomer.com',
+    email: 'ghopkins@gcustomer.com',
     locale: 'en-US',
   },
 };
