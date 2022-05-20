2719aa8c5bd2b6e00c52d8aaea689325c6aa4ce5
Pawel Kowaluk <pkowaluk@guidewire.com>
2022-05-20 11:14:03 +0200

    add http wrapper

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const { winstonLogger } = require('./controllers/loggerController');
+const http = require('http');
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
@@ -17,9 +18,50 @@ function normalizePort(val) {
   return false;
 }
 
-const port = normalizePort(process.env.PORT || '8081');
-app.set('port', port);
+function getFormattedError(error) {
+  const intro = 'Error in application runner (server.js):';
+  return `${intro}
+    CODE: ${error.code}
+    MESSAGE: ${error.message}
+    ERROR: ${JSON.stringify(error)}`;
+}
 
-app.listen(port, () => {
-  winstonLogger.notice('Running on PORT: ' + port);
-});
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    winstonLogger.error(getFormattedError(error));
+  }
+
+  var bind =
+    typeof httpServerPort === 'string'
+      ? 'Pipe ' + httpServerPort
+      : 'Port ' + httpServerPort;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      winstonLogger.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      winstonLogger.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      winstonLogger.error(getFormattedError(error));
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  winstonLogger.notice('Listening on ' + bind);
+}
+
+const httpServerPort = normalizePort(process.env.PORT || '8081');
+
+// RUN APP IN SERVER
+const server = http.createServer(app);
+server.listen(httpServerPort);
+
+server.on('error', onError);
+server.on('listening', onListening);
