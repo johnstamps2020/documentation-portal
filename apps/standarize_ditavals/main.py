d82486d6f1f70582d2936e89c42c5d0b261922ac
mskowron <mskowron@guidewire.com>
2022-06-30 15:58:51 +0200

    DOCS-2657 - Add a function for validating sources against Bitbucket

--- apps/standarize_ditavals/main.py
+++ apps/standarize_ditavals/main.py
@@ -1,9 +1,50 @@
 import json
 import logging
+import os
 from pathlib import Path
+from typing import Optional
+
+import requests
 
 CURRENT_DIR = Path(__file__).parent
 CONFIG_DIR = CURRENT_DIR / 'config'
+
+
+def find_non_existing_sources(src_definitions: list[dict]) -> list or None:
+    def get_branches(git_repo_name: str, start_at: Optional[int] = None):
+        start_number = start_at or 0
+        response = requests.get(
+            f'{bitbucket_api_root_url}/DOCSOURCES/repos/{git_repo}/branches?start={start_number}',
+            headers=bitbucket_api_headers,
+        )
+        response_json = response.json()
+        if response.ok:
+            branches = [v['displayId'] for v in response_json.get('values', [])]
+            is_last_page = response_json.get('isLastPage', True)
+            if not is_last_page:
+                branches.extend(
+                    get_branches(git_repo_name, start_at=response_json.get('nextPageStart')))
+            return branches
+        elif 'NoSuchRepositoryException' in response_json.get('errors')[0].get('exceptionName'):
+            return None
+
+    bitbucket_access_token = os.environ.get('BITBUCKET_ACCESS_TOKEN')
+    bitbucket_api_root_url = 'https://stash.guidewire.com/rest/api/1.0/projects'
+    bitbucket_api_headers = {
+        'Authorization': f'Bearer {bitbucket_access_token}'
+    }
+    missing_sources = []
+    for src in src_definitions:
+        logging.info(f'Checking the {src["id"]} source')
+        src_branch = src['branch']
+        git_repo = src['gitUrl'].split('/')[-1].replace('.git', '')
+        git_branches = get_branches(git_repo)
+        if src not in missing_sources:
+            if not git_branches or (git_branches and src_branch not in git_branches):
+                missing_sources.append(src)
+
+    (CURRENT_DIR / 'missing_sources.json').open('w').write(json.dumps(missing_sources, indent=2))
+    logging.info(f'Missing sources: {len(missing_sources)}')
 
 
 def list_common_gw_ditavals():
@@ -25,23 +66,16 @@ def load_sources():
     return scf_json['sources']
 
 
-def get_git_info_for_src(src_id: str, src_definitions: list[dict]) -> dict:
-    return next(({
-        'gitUrl': src['gitUrl'],
-        'gitBranch': src['branch']
-    } for src in src_definitions if src['id'] == src_id), None)
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    builds = load_builds()
-    sources = load_sources()
+def find_ditavals_for_update(build_definitions: list[dict], src_definitions: list[dict]):
     common_gw_ditavals = list_common_gw_ditavals()
     ditavals_to_update = []
-    for build in builds:
+    for build in build_definitions:
         if build_filter := build.get('filter'):
             if 'common-gw' not in build_filter:
-                git_src = get_git_info_for_src(build['srcId'], sources)
+                git_src = next(({
+                    'gitUrl': src['gitUrl'],
+                    'gitBranch': src['branch']
+                } for src in src_definitions if src['id'] == build['srcId']), None)
                 if matching_entry := next((entry for entry in ditavals_to_update if entry['filter'] == build_filter),
                                           None):
                     matching_git_src = next((src for src in matching_entry['gitSrcs'] if src == git_src), None)
@@ -55,3 +89,11 @@ if __name__ == '__main__':
                     })
     (CURRENT_DIR / 'ditavals_to_update.json').open('w').write(json.dumps(ditavals_to_update, indent=2))
     logging.info(f'Ditavals outside the common-gw submodule: {len(ditavals_to_update)}')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    builds = load_builds()
+    sources = load_sources()
+    # find_ditavals_for_update(builds, sources)
+    find_non_existing_sources(sources)
