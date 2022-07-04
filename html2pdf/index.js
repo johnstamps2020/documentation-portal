52cd20172f3d2c6764ebac2e281ba29d7532c842
Pawel Kowaluk <pkowaluk@guidewire.com>
2022-07-04 15:24:19 +0200

    add stub files

---
+++ html2pdf/index.js
@@ -0,0 +1,38 @@
+const { spawn } = require("node:child_process");
+const path = require("path");
+const fs = require("fs");
+
+const htmlFilesDir =
+  process.env.HTML_FILES_DIR || path.join(process.cwd(), "test-files");
+
+const inputDir = path.join(process.cwd(), "in");
+const outputDir = path.join(process.cwd(), "out");
+
+if (!fs.existsSync(htmlFilesDir)) {
+  console.error(`Base input folder does not exist: ${htmlFilesDir}`);
+  process.exit(1);
+}
+
+if (fs.existsSync(inputDir)) {
+  fs.rmSync(inputDir, { recursive: true });
+}
+
+fs.cpSync(htmlFilesDir, inputDir, { recursive: true });
+
+if (fs.existsSync(outputDir)) {
+  fs.rmSync(outputDir, { recursive: true });
+}
+
+const server = spawn("serve", [inputDir]);
+
+server.stdout.on("data", (data) => {
+  console.log(`SERVER: ${data}`);
+});
+
+server.stderr.on("data", (data) => {
+  console.error(`SERVER ERROR: ${data}`);
+});
+
+server.on("close", (code) => {
+  console.log(`Server process exited with code ${code}`);
+});
