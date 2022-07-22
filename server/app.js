6c620353a62642f445b3991f95ff6f9a20dad19e
Pawel Kowaluk <pkowaluk@guidewire.com>
2022-07-22 11:01:27 +0200

    remove the usage of sass in app.js

--- server/app.js
+++ server/app.js
@@ -10,7 +10,6 @@ const {
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const sassMiddleware = require('node-sass-middleware');
 const favicon = require('serve-favicon');
 const session = require('cookie-session');
 const httpContext = require('express-http-context');
@@ -111,14 +110,6 @@ app.use(getPage);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true,
-  })
-);
 
 app.use(httpContext.middleware);
 
