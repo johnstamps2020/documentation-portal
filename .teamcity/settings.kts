c455f6410db89b80b6c848c93d700dad47dbd9c1
mskowron <mskowron@guidewire.com>
2022-08-29 14:01:22 +0200

    DOCS-2557 - Enable server deployments to prod for andromeda-omega2
    Temporarily enable deployments from the andromeda-omega2 branch to staging and prod to be able to test deployments to the new prod environment.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2158,7 +2158,14 @@ object Server {
         }
 
         if (arrayOf(GwDeployEnvs.STAGING.env_name, GwDeployEnvs.PROD.env_name).contains(deploy_env)) {
-            deployServerBuildType.vcs.branchFilter = "+:<default>"
+            // Temporarily enable deployment to andromenda-omega2 from a branch
+            // TODO: After the migration from prod-us-east-2 is complete and changes are merged to master
+            //  The branch filter must be restored to the following value
+            //  deployServerBuildType.vcs.branchFilter = "+:<default>"
+            deployServerBuildType.vcs.branchFilter = """
+                +:<default>
+                +:(refs/heads/andromeda-omega2)
+            """.trimIndent()
             deployServerBuildType.params.text(
                 "TAG_VERSION",
                 "",
