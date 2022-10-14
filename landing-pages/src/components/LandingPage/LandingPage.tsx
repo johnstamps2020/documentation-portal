1b628366e74cc8b2810705b0d1993f528ba3ba19
Pawel Kowaluk <pkowaluk@guidewire.com>
2022-10-14 09:54:27 +0200

    add a mock config

--- landing-pages/src/components/LandingPage/LandingPage.tsx
+++ landing-pages/src/components/LandingPage/LandingPage.tsx
@@ -1,15 +1,35 @@
 import { Link, useParams } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import { mockConfig } from "./mockConfig";
+import DoneIcon from "@mui/icons-material/Done";
+import CloseIcon from "@mui/icons-material/Close";
+import Card from "@mui/material/Card";
 
 export default function LandingPage() {
   const params = useParams();
-  const pagePath = params["*"];
+  const pagePathFromRouter = params["*"];
+  const { title, pagePath, items } = mockConfig;
 
   return (
-    <Layout title="Home">
-      <Link to="/">home</Link>
-      <h1>Welcome to Guidewire, we hope you can have fun here.</h1>
-      <p>Selected path: {pagePath}</p>
+    <Layout title={title}>
+      <h1>{title}</h1>
+      <div style={{ display: "flex", alignItems: "center", fontSize: "80%" }}>
+        <div>You are here: {pagePathFromRouter} </div>
+        <div>
+          {pagePathFromRouter === pagePath ? (
+            <DoneIcon fontSize="small" />
+          ) : (
+            <CloseIcon fontSize="small" />
+          )}
+        </div>
+      </div>
+      <div>
+        {items.map((item) => (
+          <Card>
+            <h2>{item.label}</h2>
+          </Card>
+        ))}
+      </div>
     </Layout>
   );
 }
