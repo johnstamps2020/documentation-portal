9935c10a2ba6d9a662b98f10a59bac2bbfa6f471
Ryuichi Wakita <rwakita@guidewire.com>
2022-10-20 16:43:13 -0700

    add new doc categories for the l10n doc portal

--- apps/lion_page_builder/lion_page_builder/main.py
+++ apps/lion_page_builder/lion_page_builder/main.py
@@ -62,7 +62,7 @@ product_codes_labels = {
     'ipf': {'label': 'ClaimCenter Package for France'},
     'ipg': {'label': 'Global Solutions - Germany'},
     'cm': {'label': 'IS Contact Manager'},
-    'apd': {'label': 'Advanced Product Designer’}
+    'apd': {'label': 'Advanced Product Designer'}
 }
 
 
