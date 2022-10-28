1094556e9a326bdec97fcf307b6bb42758a09eb3
Pawel Kowaluk <pkowaluk@guidewire.com>
2022-10-28 14:55:25 +0200

    implement add and delete

--- server/controllers/s3Controller.js
+++ server/controllers/s3Controller.js
@@ -1,18 +1,64 @@
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 const bucketParams = {
   Bucket: `tenant-doctools-${process.env.DEPLOY_ENV}-builds`,
 };
 
-async function listItems(path) {
+async function listItems(prefix) {
   try {
-    const result = await s3
-      .listObjects({ ...bucketParams, Prefix: path })
-      .promise();
-    return result;
+    return s3.listObjects({ ...bucketParams, Prefix: prefix }).promise();
   } catch (err) {
     throw new Error(err);
   }
 }
 
-module.exports = { listItems };
+async function addItems(filesFromClient, prefix) {
+  try {
+    const fileResults = [];
+    const filesIterable = (function() {
+      if (Array.isArray(filesFromClient)) {
+        return filesFromClient;
+      } else {
+        return [filesFromClient];
+      }
+    })();
+    for await (const file of filesIterable) {
+      fileResults.push(
+        await s3
+          .upload({
+            ...bucketParams,
+            Body: file.data,
+            Key: path.join(prefix, file.name),
+          })
+          .promise()
+      );
+    }
+
+    return fileResults;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+async function deleteItems(keysCommaSeparated) {
+  try {
+    const keys = keysCommaSeparated.split(',');
+
+    const deleteResults = [];
+
+    for await (const key of keys) {
+      const result = await s3
+        .deleteObject({ ...bucketParams, Key: key })
+        .promise();
+      deleteResults.push(result);
+    }
+
+    return deleteResults;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+module.exports = { listItems, addItems, deleteItems };
--- server/routes/s3.js
+++ server/routes/s3.js
@@ -1,28 +1,82 @@
 const express = require('express');
 const router = express.Router();
-const { getConfig } = require('../controllers/configController');
 const { winstonLogger } = require('../controllers/loggerController');
-const { listItems } = require('../controllers/s3Controller');
+const {
+  listItems,
+  addItems,
+  deleteItems,
+} = require('../controllers/s3Controller');
+
+const fileUpload = require('express-fileupload');
+router.use(fileUpload());
+
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', async function(req, res, next) {
   try {
     const { path } = req.query;
 
     if (!path) {
-      res.status(500).send({
+      return res.status(500).send({
         message: 'Error! Path is required',
       });
     }
 
     const result = await listItems(path);
-    res.status(200).send({
-      result,
+    return res.status(200).send({
+      ...result,
     });
   } catch (err) {
     winstonLogger.error(
-      `[SAFE CONFIG] Problem sending config from ${req.url}: ${JSON.stringify(
-        err
-      )}`
+      `[S3 controller] Problem listing items: ${JSON.stringify(err)}`
+    );
+    next(err);
+  }
+});
+
+router.post('/', async function(req, res, next) {
+  try {
+    const { path } = req.query;
+    if (!path) {
+      return res.status(500).send({
+        message: 'Error! Path is required',
+      });
+    }
+
+    const { filesFromClient } = req.files;
+
+    if (!filesFromClient) {
+      return res.status(500).send({
+        message: 'Error! No files attached',
+      });
+    }
+
+    const result = await addItems(filesFromClient, path);
+    return res.status(200).send(result);
+  } catch (err) {
+    winstonLogger.error(
+      `[S3 controller] Problem adding an item: ${JSON.stringify(err)}`
+    );
+    next(err);
+  }
+});
+
+router.delete('/', async function(req, res, next) {
+  try {
+    const { keys } = req.query;
+    if (!keys) {
+      return res.status(500).send({
+        message: 'Error! Keys are required',
+      });
+    }
+
+    const result = await deleteItems(keys);
+    return res.status(200).send(result);
+  } catch (err) {
+    winstonLogger.error(
+      `[S3 controller] Problem deleting item(s): ${JSON.stringify(err)}`
     );
     next(err);
   }
