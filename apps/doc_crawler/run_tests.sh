50049cf492e16bf2d9e831dc0a5ffc03c2c46e1d
mskowron <mskowron@guidewire.com>
2022-10-28 11:48:16 +0200

    DOCS-2049 - Update tests

--- apps/doc_crawler/run_tests.sh
+++ apps/doc_crawler/run_tests.sh
@@ -5,7 +5,11 @@ export CONFIG_FILE=./tests/test_doc_crawler/resources/input/config/gw-docs.json 
 		&& export APP_BASE_URL=http://localhost/ \
 		&& export DOC_S3_URL=http://localhost/ \
 		&& export ELASTICSEARCH_URLS=http://localhost:9200 \
-		&& export INDEX_NAME=gw-docs \
+		&& export DOCS_INDEX_NAME=gw-docs \
+		&& export BROKEN_LINKS_INDEX_NAME=broken-links \
+		&& export SHORT_TOPICS_INDEX_NAME=short-topics \
+		&& export REPORT_BROKEN_LINKS=yes \
+		&& export REPORT_SHORT_TOPICS=yes \
 		&& apt-get update && apt-get install -y build-essential \
 		&& pip install poetry \
         && poetry install \
