e2e8b490d3f998116c5d7e91135567e26af48f24
Maria Szeligiewicz <mszeligiewicz@guidewire.com>
2022-11-15 11:35:30 +0100

    finished footer

--- landing-pages/src/components/Layout/Layout.tsx
+++ landing-pages/src/components/Layout/Layout.tsx
@@ -1,9 +1,22 @@
+import { layoutTheme } from "../../themes/layoutTheme";
 import Logo from "../Logo/Logo";
 import SearchBox from "../SearchBox/SearchBox";
-import styles from "./Layout.module.css";
 import AppBar from "@mui/material/AppBar";
-import { BottomNavigation, Link, Typography } from "@mui/material";
-import { Container } from "@mui/system";
+import {
+  Avatar,
+  Badge,
+  BottomNavigation,
+  Box,
+  CardMedia,
+  CssBaseline,
+  ImageList,
+  ImageListItem,
+  Link,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import elysianBadge from "../../images/badge-elysian.svg";
 
 type LayoutProps = {
   children: JSX.Element | JSX.Element[];
@@ -20,64 +33,39 @@ export default function Layout({
 
   return (
     <div>
-      <AppBar
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexWrap: "wrap",
-          height: "80px",
-          padding: "6px",
-          backgroundColor: "hsl(216, 42%, 13%)",
-        }}
-      >
-        <Logo />
-        <SearchBox />
-      </AppBar>
-      <main>{children}</main>
-      <BottomNavigation
-        sx={{
-          backgroundColor: "hsl(216, 42%, 13%)",
-          color: "hsl(0, 0%, 98%)",
-          display: "flex",
-          alignItems: "left",
-        }}
-      >
-        <Container sx={{ display: "flex", marginLeft: 1 }}>
-          <Typography
-            variant="h4"
-            style={{
-              fontSize: "0.75rem",
-              fontWeight: 400,
-              paddingTop: 20,
-              marginRight: "50%",
-            }}
-          >
-            {"Copyright 2022 Guidewire Software, Inc."}
-          </Typography>
-          <Link
-            href="/support"
-            sx={{
-              fontSize: "0.75rem",
-              fontWeight: 400,
-              paddingTop: "20px",
-              marginRight: "75%",
-            }}
-          >
-            Legal and Support Information
-          </Link>
-          <Typography
-            variant="h4"
-            style={{
-              fontSize: "0.75rem",
-              fontWeight: 400,
-              paddingTop: 20,
-            }}
-          >
-            {"Elysian Release"}
-          </Typography>
-        </Container>
-      </BottomNavigation>
+      <ThemeProvider theme={layoutTheme}>
+        <CssBaseline enableColorScheme />
+        <AppBar>
+          <Logo />
+          <SearchBox />
+        </AppBar>
+        <main>{children}</main>
+        <BottomNavigation>
+          <Box sx={{ display: "flex", width: "100%" }}>
+            <Typography
+              variant="h6"
+              style={{ marginRight: "33%", marginLeft: "2%" }}
+            >
+              {"Copyright 2022 Guidewire Software, Inc."}
+            </Typography>
+            <Link href="/support">Legal and Support Information</Link>
+            <ImageList>
+              <ImageListItem>
+                <img
+                  src={elysianBadge}
+                  alt="elysian-badge-logo"
+                  style={{
+                    height: "20px",
+                  }}
+                />
+              </ImageListItem>
+            </ImageList>
+            <Typography variant="h6" style={{ marginRight: "2%" }}>
+              {"Elysian Release"}
+            </Typography>
+          </Box>
+        </BottomNavigation>
+      </ThemeProvider>
     </div>
   );
 }
--- landing-pages/src/themes/layoutTheme.ts
+++ landing-pages/src/themes/layoutTheme.ts
@@ -1,5 +1,58 @@
 import { createTheme } from "@mui/material";
 
-export const landingPageTheme = createTheme({
+export const layoutTheme = createTheme({
+  typography: {
+    fontFamily: ["Source Sans Pro", "Helvetica", "Arial", "sans-serif"].join(
+      ","
+    ),
+    h6: {
+      fontSize: 11,
+      fontWeight: 600,
+      marginTop: "22px",
+    },
+  },
 
-})
+  components: {
+    MuiAppBar: {
+      defaultProps: {
+        sx: {
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          height: "80px",
+          padding: "6px",
+          backgroundColor: "hsl(216, 42%, 13%)",
+        },
+      },
+    },
+    MuiBottomNavigation: {
+      defaultProps: {
+        sx: {
+          backgroundColor: "hsl(216, 42%, 13%)",
+          color: "hsl(0, 0%, 98%)",
+          display: "flex",
+          alignItems: "left",
+        },
+      },
+    },
+    MuiLink: {
+      defaultProps: {
+        underline: "none",
+        sx: {
+          fontSize: 11,
+          fontWeight: 600,
+          marginTop: "22px",
+          marginRight: "33%",
+          color: "white",
+        },
+      },
+    },
+    MuiImageList: {
+      defaultProps: {
+        cols: 1,
+        style: { marginRight: "2px", marginLeft: "auto" },
+      },
+    },
+  },
+});
