39494f2966dc794d4b49c36f27f756d0886ba3d3
mskowron <mskowron@guidewire.com>
2023-02-03 09:04:42 +0100

    DOCS-2981 - Move the build script to a file
    Extra: Remove patches created by manual changes in the TeamCity UI

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -250,29 +250,9 @@ object Database {
                     id = Helpers.createIdStringFromName(this.name)
                     scriptContent = """
                         #!/bin/bash
-                        set -xe
                         
-                        # TODO: When this build is productized, add conditions for settings proper vars depending on env
-                        APP_BASE_URL="https://croissant.dev.ccs.guidewire.net"
-                        OKTA_ACCESS_TOKEN_ISSUER=https://guidewire-hub.oktapreview.com/oauth2/ausj9ftnbxOqfGU4U0h7
-                        BASE64_CLIENT_CREDS=${'$'}(echo -n "%env.OKTA_CLIENT_ID%:%env.OKTA_CLIENT_SECRET%" | base64)
-
-                        JWT=${'$'}(curl --location -X POST "${'$'}OKTA_ACCESS_TOKEN_ISSUER/v1/token" \
-                          --header "Content-Type: application/x-www-form-urlencoded" \
-                          --header "Authorization: Basic ${'$'}BASE64_CLIENT_CREDS" \
-                          --data-urlencode "grant_type=client_credentials" \
-                          --data-urlencode "scope=%env.OKTA_ACCESS_TOKEN_SCOPES%" |
-                          jq -r '.access_token')
-
-                        [[ -z "${'$'}JWT" || "${'$'}JWT" == "null" ]] && echo "Unable to get an access token from Okta" && exit 1
-                        
-                        for entityType in source doc page openRoutes
-                        do
-                            curl -o response_${'$'}{entityType}.json -X PUT "${'$'}APP_BASE_URL/safeConfig/entity/legacy/putConfigInDatabase/${'$'}{entityType}" \
-                              --header "Authorization: Bearer ${'$'}JWT"
-
-                            echo "Done. For details, see the response_${'$'}{entityType}.json file in the build artifacts."
-                        done
+                        cd %teamcity.build.checkoutDir%/ci
+                        ./uploadLegacyConfigsToDb.sh
                         """.trimIndent()
                 }
             }
