4cd207a7dc94293c5391824ae3a5c116c565bf4d
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-02-09 13:41:26 +0100

    remove python apps

--- apps/config_deployer/build_docker.sh
+++
@@ -1,2 +0,0 @@
-cp ../../.teamcity/config/config-schema.json config_deployer/
-docker image build --no-cache -t config-deployer .
--- apps/config_deployer/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-
-pip install poetry \
-    && poetry install \
-    && poetry run python -m pytest -v ./tests/test_config_deployer.py
