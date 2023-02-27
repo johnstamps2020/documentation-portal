118df44aa24c98c757e131b7cf024a9fd5f8cbfc
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-02-27 14:30:49 +0100

    add prettier

--- landing-pages/src/components/LandingPage/LandingPageSidebar.tsx
+++ landing-pages/src/components/LandingPage/LandingPageSidebar.tsx
@@ -1,20 +1,20 @@
-import Stack from "@mui/material/Stack";
-import LandingPageItem from "./LandingPageItem";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
+import Stack from '@mui/material/Stack';
+import LandingPageItem from './LandingPageItem';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 
 export default function LandingPageSidebar(sidebar: {}) {
   return (
     <Paper
       sx={{
-        height: "fit-content",
-        minHeight: "180px",
-        minWidth: "270px",
+        height: 'fit-content',
+        minHeight: '180px',
+        minWidth: '270px',
         width: {
-          sm: "fit-content",
-          xs: "100%"
+          sm: 'fit-content',
+          xs: '100%',
         },
-        padding: "24px"
+        padding: '24px',
       }}
     >
       <Typography variant="h2">Dummy label</Typography>
