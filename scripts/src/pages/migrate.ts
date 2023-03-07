a9f208f680715ff032f3fd8ef4e6d7188f9f0de6
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-03-07 11:43:49 +0100

    add class mapping

--- scripts/src/pages/migrate.ts
+++ scripts/src/pages/migrate.ts
@@ -1,7 +1,16 @@
 import { getAllFilesRecursively } from '../modules/fileOperations';
 import { resolve } from 'path';
+import { writeFileSync } from 'fs';
 
 const landingPagesSourceDir = resolve(__dirname, '../../../frontend/pages');
 
+// class on the 1st second-level item
+const classMap = {
+  categoryCard: 'CategoryLayout',
+  categoryCard2: 'Category2Layout', // EXCEPTION: when main class contains `garmisch` or `flaine`
+  productFamily: 'ProductFamilyLayout',
+  subject: 'SectionLayout',
+};
+
 const allFiles = getAllFilesRecursively(landingPagesSourceDir, '.json');
-console.log(allFiles);
+writeFileSync('allFiles.json', JSON.stringify(allFiles, null, 2));
