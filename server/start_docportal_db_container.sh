154691746c5dfa3671c359e8b63ab70a7ed8158f
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-03-07 14:43:42 +0100

    make the script start without collisions

--- server/start_docportal_db_container.sh
+++ server/start_docportal_db_container.sh
@@ -2,10 +2,16 @@ echo "!!!!!!!!!!!!!!!!!!!"
 echo "If the DB does not work, try removing your local postgres files `rm -rf /private/tmp/pgdata`"
 echo "!!!!!!!!!!!!!!!!!!!"
 
-docker run --name docportal-db \
-  -e POSTGRES_USER=postgres \
-  -e POSTGRES_PASSWORD=testtesttest \
-  -e PGDATA=/var/lib/postgresql/data/pgdata \
-  -v /tmp:/var/lib/postgresql/data \
-  -p 5432:5432 \
-  postgres:14.1-alpine
+export container_name="docportal-db"
+
+if [ "$( docker container inspect -f '{{.State.Status}}' $container_name )" == "running" ]; then
+  docker run --name $container_name \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_PASSWORD=testtesttest \
+    -e PGDATA=/var/lib/postgresql/data/pgdata \
+    -v /tmp:/var/lib/postgresql/data \
+    -p 5432:5432 \
+    postgres:14.1-alpine
+else
+  docker start $container_name -a
+fi
