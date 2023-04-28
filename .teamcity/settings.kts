f9e8428aade17e44a4d9fc6ed3cec4ba08b41a88
mskowron <mskowron@guidewire.com>
2023-04-28 12:19:34 +0200

    DOCS-2921 - Add missing break

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -251,6 +251,7 @@ object Database {
                       status=${'$'}(kubectl get pods $podName -o jsonpath='{.status.phase}')
                       if [ "${'$'}status" == "Running" ]; then
                         kubectl exec $podName -- sh -c "apk add --no-cache postgresql-client zip && pg_dump -Fd %env.CONFIG_DB_NAME% -j 5 -f %env.CONFIG_DB_NAME% && zip -r $dbDumpZipPackageName %env.CONFIG_DB_NAME%" && kubectl cp $podName:/$dbDumpZipPackageName ./$dbDumpZipPackageName || EXIT_CODE=${'$'}?
+                        break
                       else
                         echo "Waiting for the $podName pod to be ready"
                         sleep 5
@@ -283,7 +284,8 @@ object Database {
                     while [ ${'$'}SECONDS -le 30 ]; do
                       status=${'$'}(kubectl get pods $podName -o jsonpath='{.status.phase}')
                       if [ "${'$'}status" == "Running" ]; then
-                        kubectl cp ./$dbDumpZipPackageName $podName:/$dbDumpZipPackageName && kubectl exec $podName -- sh -c "apk add --no-cache postgresql-client zip && unzip ./$dbDumpZipPackageName && pg_restore --clean --if-exists -d %env.CONFIG_DB_NAME% %env.CONFIG_DB_NAME%" || EXIT_CODE=${'$'}? 
+                        kubectl cp ./$dbDumpZipPackageName $podName:/$dbDumpZipPackageName && kubectl exec $podName -- sh -c "apk add --no-cache postgresql-client zip && unzip ./$dbDumpZipPackageName && pg_restore --clean --if-exists -d %env.CONFIG_DB_NAME% %env.CONFIG_DB_NAME%" || EXIT_CODE=${'$'}?
+                        break
                       else
                         echo "Waiting for the $podName pod to be ready"
                         sleep 5
