d47f31ab2b423fa131539f66768592c5cec79514
mskowron <mskowron@guidewire.com>
2023-05-09 16:23:31 +0200

    (ad-hoc) Fix the script for configuring redirect from int to staging

--- server/kube/configure-redirect-int-to-staging.sh
+++ server/kube/configure-redirect-int-to-staging.sh
@@ -6,5 +6,5 @@
 #    by copying and pasting them into the terminal and then pressing Enter.
 # 2. Run this script to configure the nginx server.
 
-kubectl exec -it "$(kubectl get pods -l app=redirect-int-to-staging -o jsonpath='{.items[0].metadata.name}')" -n doctools \
+kubectl exec -it "$(kubectl get pods -l app=redirect-int-to-staging -o jsonpath='{.items[0].metadata.name}' -n doctools)" -n doctools \
  -- /bin/sh -c 'rm /etc/nginx/conf.d/default.conf && echo "server { listen 80; listen 443; server_name _; return 301 https://docs.staging.ccs.guidewire.net$request_uri; }" > /etc/nginx/conf.d/nginx.conf && nginx -s reload'
