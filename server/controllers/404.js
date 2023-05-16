5be18b5fcd4a24376834406180055e6ec22ad3ec
Miles Gilster <mgilster@guidewire.com>
2023-05-16 13:50:14 -0700

    remove stale redirects

--- server/controllers/404.js
+++ server/controllers/404.js
@@ -10,10 +10,6 @@ const redirectUrls = [
     to: 'cloudProducts/dataPlatform',
   },
   {
-    from: 'cloud/restapi/latest/release-notes',
-    to: 'cloud/gw-feature-preview/igrelnotes',
-  },
-  {
     from: 'cloudProducts/Aspen',
     to: 'cloudProducts/aspen',
   },
@@ -26,44 +22,12 @@ const redirectUrls = [
     to: 'cloudProducts/cortina',
   },
   {
-    from: 'cloud/gw-feature-preview/latest',
-    to: 'cloudProducts/cortina/featurePreview',
-  },
-  {
     from: 'jutro/documentation/4.1.1',
     to: 'jutroDesignSystem/4.1.1',
   },
   {
     from: 'selfManagedProducts/jutroDesignSystem/4.1.1',
     to: 'jutroDesignSystem/4.1.1',
-  },
-  {
-    from: 'cloudProducts/Aspen/billingCenterCloud/explore/latest',
-    to: 'cloudProducts/explore/latest',
-  },
-  {
-    from: 'cloudProducts/Aspen/claimCenterCloud/explore/latest',
-    to: 'cloudProducts/explore/latest',
-  },
-  {
-    from: 'cloudProducts/Aspen/policyCenterCloud/explore/latest',
-    to: 'cloudProducts/explore/latest',
-  },
-  {
-    from: 'cloudProducts/Aspen/insuranceNow/IN20201xExt/latest',
-    to: 'cloudProducts/aspen/insuranceNow/2020.1xExternalAudience',
-  },
-  {
-    from: 'cloudProducts/Aspen/insuranceNow/IN20201xInt/latest',
-    to: 'cloudProducts/aspen/insuranceNow/2020.1xInternalAudience',
-  },
-  {
-    from: 'cloudProducts/Aspen/insuranceNow/insNowAspen/2020.1',
-    to: 'cloudProducts/aspen/insuranceNow/2020.1',
-  },
-  {
-    from: 'cloudProducts/Aspen/guidewireCloudPlatform/gwCloudConsole/latest',
-    to: 'cloudProducts/aspen/guidewireCloudPlatform',
   },
   {
     from: 'cloudProducts/Banff/billingCenterCloud/explore/latest',
@@ -103,7 +67,7 @@ const redirectUrls = [
   },
   {
     from: 'cloudProducts/Banff/guidewireCloudPlatform/gwCloudConsole/latest',
-    to: 'cloudProducts/banff/guidewireCloudPlatform',
+    to: 'cloudProducts/guidewireCloudPlatform',
   },
   {
     from: 'cloudProducts/Cortina/billingCenterCloud/jutroDesignSystem/5.3.0',
@@ -154,60 +118,20 @@ const redirectUrls = [
     to: 'cloudProducts/cortina/insuranceNow/2021.1',
   },
   {
-    from: 'cloud/bc/202011/cloud/active/config/topics/p-basics.html',
-    to: 'cloud/bc/202104/config',
-  },
-  {
-    from: 'cloud/cc/202011/cloud/active/config/topics/p-basics.html',
-    to: 'cloud/cc/202104/config',
-  },
-  {
-    from: 'cloud/pc/202011/cloud/active/config/topics/p-basics.html',
-    to: 'cloud/pc/202104/config',
-  },
-  {
-    from: 'cloud/bc/config/latest',
-    to: 'cloud/bc/202104/config',
-  },
-  {
-    from: 'cloud/cc/config/latest',
-    to: 'cloud/cc/202104/config',
-  },
-  {
-    from: 'cloud/pc/config/latest',
-    to: 'cloud/pc/202104/config',
-  },
-  {
-    from: 'cloud/pc/202011/cloud/active/adv-product-designer/topics/c_intro.html',
-    to: 'cloud/pc/202104/apd',
-  },
-  {
-    from: 'cloud/pc/apd/latest',
-    to: 'cloud/pc/202104/apd',
-  },
-  {
-    from: 'cloud/in/20202/2020.2.x/active/ConfigurationGuide/c_Overview.html',
-    to: 'cloud/in/20211/config',
-  },
-  {
-    from: 'cloud/in/config/latest',
-    to: 'cloud/in/20211/config',
-  },
-  {
     from: 'cloud/bc/202011/cloud/active/gosu/topics/c_p-basics.html',
-    to: 'cloud/is/202104/gosu',
+    to: 'is/gosu',
   },
   {
     from: 'cloud/cc/202011/cloud/active/gosu/topics/c_p-basics.html',
-    to: 'cloud/is/202104/gosu',
+    to: 'is/gosu',
   },
   {
     from: 'cloud/pc/202011/cloud/active/gosu/topics/c_p-basics.html',
-    to: 'cloud/is/202104/gosu',
+    to: 'is/gosu',
   },
   {
     from: 'cloud/is/gosu/latest',
-    to: 'cloud/is/202104/gosu',
+    to: 'is/gosu',
   },
   {
     from: 'cloud/in/20202/config/2020.2.x/active/ConfigurationGuide/CloudCommonPaymentService/c_CloudCommonPaymentServiceconfiguration.html',
@@ -219,19 +143,15 @@ const redirectUrls = [
   },
   {
     from: 'cloud/in/20202/2020.2.x/active/PortalDevelopment/topics/c_overview_portal_development.html',
-    to: 'cloud/in/20211/portaldev',
+    to: 'cloud/in/latest/portaldev',
   },
   {
     from: 'cloud/in/portaldev/latest',
-    to: 'cloud/in/20211/portaldev',
+    to: 'cloud/in/latest/portaldev',
   },
   {
     from: 'cloud/is/202011/restapiclient/REST-API-Client/topics/c_overview-rest-client.html',
     to: 'is/restapiclient/guide',
-  },
-  {
-    from: 'jutro/documentation/latest',
-    to: 'jutro/documentation/5.4.0',
   },
   {
     from: 'portal/secure/upgradediff',
