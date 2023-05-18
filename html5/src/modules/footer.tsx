a3816f3566ce7a5b8580ffef282e821ef5d8782d
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-05-18 18:01:07 +0200

    translate the footer center

---
+++ html5/src/modules/footer.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import '../stylesheets/modules/footer.css';
+import Translate from '@theme/Translate';
+
+type FooterPublicationDateProps = {
+  publicationTime: string;
+};
+
+function FooterPublicationDate({
+  publicationTime,
+}: FooterPublicationDateProps) {
+  return (
+    <div id="publicationTime">
+      <Translate id="footer.published">Published:</Translate> {publicationTime}
+    </div>
+  );
+}
+
+export function addFooterContents(isOffline: boolean) {
+  const footerTemplate = `<div>
+        <div class="footerLinks">
+            <span class="footerLink">
+                <a href="${
+                  isOffline ? 'https://docs.guidewire.com' : ''
+                }/support" ${
+    isOffline ? 'target="__blank" rel="noopener noreferrer"' : ''
+  }>Legal and Support Information</a>
+            </span>
+        </div>
+        <div class="footerCopyright">
+            Copyright ©${new Date().getFullYear()}
+            Guidewire Software, Inc.
+        </div>
+    </div>`;
+
+  const footerRight = document.getElementById('footerRight');
+  footerRight.innerHTML = footerTemplate;
+
+  const footerCenter = document.getElementById('footerCenter');
+  const pubDate = footerCenter.innerText.match(/Published: (.*)/)[1];
+  const footerCenterRoot = createRoot(footerCenter);
+  footerCenterRoot.render(<FooterPublicationDate publicationTime={pubDate} />);
+}
