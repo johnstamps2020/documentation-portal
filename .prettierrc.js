0b7affb54b273a9be8a83720de9249438e2d425a
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-05-18 14:14:06 +0200

    add VS Code launch scripts

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,34 @@
 module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
   singleQuote: true,
   trailingComma: 'es5',
-  tabWidth: 2,
+  proseWrap: 'always',
+  overrides: [
+    {
+      files: '*.json5',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: '*.json',
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: '*.mdx',
+      options: {
+        proseWrap: 'always',
+      },
+    },
+    {
+      files: '*.md',
+      options: {
+        proseWrap: 'always',
+      },
+    },
+  ],
 };
