da52b758babc53ba6682709758018b89f9228fcc
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-05-26 16:22:44 +0200

    update styles of admin pages

--- landing-pages/src/components/PageAdminPage/PageList.tsx
+++ landing-pages/src/components/PageAdminPage/PageList.tsx
@@ -15,7 +15,12 @@ type PageListProps = {
 
 export default function PageList({ pages }: PageListProps) {
   return (
-    <Grid container sx={{ margin: '8px 0' }}>
+    <Grid
+      container
+      padding={1}
+      spacing={2}
+      sx={{ backgroundColor: 'lightgray' }}
+    >
       {pages.map(({ path, title }) => (
         <Grid key={path} xs={12} sm={6} md={3}>
           <Card sx={{ padding: 1, height: '100%' }}>
