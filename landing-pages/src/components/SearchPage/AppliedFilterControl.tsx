005483068932185ed371548cecdfe51d0fa76b99
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-05-30 14:15:20 +0200

    make filter deletable

---
+++ landing-pages/src/components/SearchPage/AppliedFilterControl.tsx
@@ -0,0 +1,22 @@
+import Chip from '@mui/material/Chip';
+import { useQueryParameters } from 'hooks/useQueryParameters';
+
+type AppliedFilterControlProps = {
+  name: string;
+  value: string;
+};
+
+export default function AppliedFilterControl({
+  name,
+  value,
+}: AppliedFilterControlProps) {
+  const { modifyFilterValue } = useQueryParameters();
+
+  function handleDelete() {
+    modifyFilterValue(name, value, false);
+  }
+
+  return (
+    <Chip size="small" label={value} onDelete={handleDelete} color="primary" />
+  );
+}
