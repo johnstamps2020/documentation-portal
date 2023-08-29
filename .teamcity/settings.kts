cef6f0ced6dc8a6d586eaad59cccf9dc9bfa5856
mskowron <mskowron@guidewire.com>
2023-08-29 15:02:24 +0200

    DOCS-4516 - Update deployment pipeline for Croissant
    - Fix checkout rules and VCS triggers for the build that test setting.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -188,7 +188,7 @@ enum class GwCheckoutRules(val ruleValue: String) {
     LANDING_PAGES_KUBE("+:landing-pages/kube"),
     SERVER("+:server"),
     SERVER_KUBE("+:server/kube"),
-    TEAMCITY("+:.teamcity"),
+    TEAMCITY_SETTINGS_KTS("+:.teamcity/settings.kts"),
     TEAMCITY_CONFIG("+:.teamcity/config"),
     CI("+:ci"),
     HTML5("+:html5"),
@@ -3783,7 +3783,11 @@ object GwBuilds {
         id = Helpers.resolveRelativeIdFromIdString(Helpers.md5(this.name))
 
         vcs {
-            root(GwVcsRoots.DocumentationPortalGitVcsRoot, GwCheckoutRules.TEAMCITY.ruleValue)
+            root(
+                GwVcsRoots.DocumentationPortalGitVcsRoot,
+                GwCheckoutRules.TEAMCITY_SETTINGS_KTS.ruleValue,
+                GwCheckoutRules.TEAMCITY_CONFIG.ruleValue
+            )
             cleanCheckout = true
         }
 
@@ -3805,8 +3809,7 @@ object GwBuilds {
         // This test build doesn't have an associated deployment build,
         // therefore we want to run it on changes in the default branch as well.
         triggers {
-            trigger(GwVcsTriggers.createDocPortalVcsTrigger(".teamcity/settings.kts"))
-            trigger(GwVcsTriggers.createDocPortalVcsTrigger(".teamcity/config/**"))
+            trigger(GwVcsTriggers.createDocPortalVcsTrigger())
         }
     })
 
