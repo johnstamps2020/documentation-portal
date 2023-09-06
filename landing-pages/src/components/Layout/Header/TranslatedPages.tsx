9c901902141765cfbafc604581bffd92988ecb80
Miles Gilster <mgilster@guidewire.com>
2023-09-06 11:57:36 -0700

    update links

--- landing-pages/src/components/Layout/Header/TranslatedPages.tsx
+++ landing-pages/src/components/Layout/Header/TranslatedPages.tsx
@@ -17,14 +17,14 @@ export default function TranslatedPages() {
   );
 
   const pages = [
-    { label: 'Deutsch', url: '/l10n-new/de-DE' },
-    { label: 'Español (España)', url: '/l10n-new/es-ES' },
-    { label: 'Español', url: '/l10n-new/es-LA' },
-    { label: 'Français', url: '/l10n-new/fr-FR' },
-    { label: 'Italiano', url: '/l10n-new/it-IT' },
-    { label: '日本語', url: '/l10n-new/ja-JP' },
-    { label: 'Nederlands', url: '/l10n-new/nl-NL' },
-    { label: 'Português', url: '/l10n-new/pt-BR' },
+    { label: 'Deutsch', url: '/l10n/de-DE' },
+    { label: 'Español (España)', url: '/l10n/es-ES' },
+    { label: 'Español', url: '/l10n/es-419' },
+    { label: 'Français', url: '/l10n/fr-FR' },
+    { label: 'Italiano', url: '/l10n/it-IT' },
+    { label: '日本語', url: '/l10n/ja-JP' },
+    { label: 'Nederlands', url: '/l10n/nl-NL' },
+    { label: 'Português', url: '/l10n/pt-BR' },
   ];
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
