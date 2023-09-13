9b8872288e533d592027a3e72ab685bb3eaf20ce
mskowron <mskowron@guidewire.com>
2023-09-13 17:42:43 +0200

    Revert "DOCS-4908 - Simulate prod on staging for testing"
    This reverts commit 9a0d1930c42f409a26debc31f67e4261891ab8d3.

--- server/src/controllers/s3Controller.ts
+++ server/src/controllers/s3Controller.ts
@@ -6,10 +6,8 @@ import path from 'path';
 import { Readable } from 'stream';
 
 const s3 = new S3({ apiVersion: '2006-03-01' });
-// TODO: Hardcoding the bucket name to staging to simulate prod on staging.
-//  Revert this change after testing is done
 const bucketParams = {
-  Bucket: `tenant-doctools-staging-builds`,
+  Bucket: `tenant-doctools-${process.env.DEPLOY_ENV}-builds`,
 };
 
 export async function getConfigFile(
