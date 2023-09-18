b65428522491dfe745a8f581db52077b2d2b66db
mskowron <mskowron@guidewire.com>
2023-09-18 11:27:57 +0200

    DOCS-4952 - Add React components for upgrade diffs

---
+++ landing-pages/src/pages/landing/upgradediffs/BillingCenter/Upgrade From 50.5.2.tsx
@@ -0,0 +1,65 @@
+import ProductFamilyLayout, {
+  ProductFamilyLayoutProps,
+} from 'components/LandingPage/ProductFamily/ProductFamilyLayout';
+import { baseBackgroundProps } from 'pages/LandingPage/LandingPageTypes';
+import { allSelectors } from 'pages/landing/selectors/allSelectors';
+
+const pageConfig: ProductFamilyLayoutProps = {
+  backgroundProps: {
+    ...baseBackgroundProps,
+    backgroundColor: `hsl(0, 0%, 98%)`,
+  },
+  selector: {
+    label: 'Select version to upgrade from',
+    selectedItemLabel: 'Upgrade From 50.5.2',
+    items: allSelectors.s65a8f50f5baaf3e8c91f86c0e3b5c1f1,
+    labelColor: 'white',
+  },
+
+  items: [
+    {
+      label: 'Upgrade To 50.8.0',
+      url: '/upgradediffs/BillingCenter/Upgrade%20From%2050.5.2/Upgrade%20To%2050.8.0',
+      videoIcon: false,
+    },
+    {
+      label: 'Upgrade To 50.7.0',
+      url: '/upgradediffs/BillingCenter/Upgrade%20From%2050.5.2/Upgrade%20To%2050.7.0',
+      videoIcon: false,
+    },
+    {
+      label: 'Upgrade To 50.6.0',
+      url: '/upgradediffs/BillingCenter/Upgrade%20From%2050.5.2/Upgrade%20To%2050.6.0',
+      videoIcon: false,
+    },
+  ],
+  sidebar: {
+    label: 'Implementation Resources',
+    items: [
+      {
+        label: 'Community Case Templates',
+        docId: 'cloudtickettemplates',
+      },
+      {
+        label: 'Product Adoption',
+        docId: 'surepathmethodologymain',
+      },
+      {
+        label: 'Cloud Standards',
+        docId: 'standardslatest',
+      },
+      {
+        label: 'Upgrade Diff Reports',
+        pagePath: 'upgradediffs',
+      },
+      {
+        label: 'Internal docs',
+        docId: 'internaldocslatest',
+      },
+    ],
+  },
+};
+
+export default function UpgradeFrom5052() {
+  return <ProductFamilyLayout {...pageConfig} />;
+}
