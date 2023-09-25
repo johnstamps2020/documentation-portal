d64f0b8acf932461be55af99182a0ccc29b89c9e
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-09-25 11:09:35 +0200

    chore: remove easter egg (_)

--- landing-pages/src/components/LoginPage/LoginInProgress.tsx
+++ landing-pages/src/components/LoginPage/LoginInProgress.tsx
@@ -1,5 +1,4 @@
 import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 
 type LoginInProgressProps = {
@@ -13,10 +12,7 @@ export default function LoginInProgress({
       sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
       open={loginIsInProgress}
     >
-      <Box sx={{ position: 'relative' }}>
-        <CircularProgress color="inherit" />
-        <Box sx={{ position: 'absolute', top: 10, left: 10 }}>🥐</Box>
-      </Box>
+      <CircularProgress color="inherit" />
     </Backdrop>
   );
 }
