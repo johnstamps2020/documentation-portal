14d3075276f29b6babdf04c8b322f2ab46f39bbe
Miles Gilster <mgilster@guidewire.com>
2023-10-02 11:17:24 -0700

    DOCS-5084 APD App Innsbruck

---
+++ landing-pages/src/pages/landing/cloudProducts/innsbruck/apd.tsx
@@ -0,0 +1,57 @@
+import SectionLayout, {
+  SectionLayoutProps,
+} from 'components/LandingPage/Section/SectionLayout';
+import { baseBackgroundProps } from 'pages/LandingPage/LandingPageTypes';
+
+const pageConfig: SectionLayoutProps = {
+  backgroundProps: {
+    ...baseBackgroundProps,
+    backgroundColor: `hsl(0, 0%, 98%)`,
+  },
+  sections: [
+    {
+      label: 'Release Notes',
+      items: [
+        {
+          label: 'APD App Release Notes',
+          docId: 'apdapprninnsbruck',
+        },
+      ],
+    },
+    {
+      label: 'Features and functionality',
+      items: [
+        {
+          label: 'APD App Guide',
+          docId: 'apdappinnsbruck',
+        },
+        {
+          label: 'APD App Guide',
+          docId: 'apdappinternalinnsbruck (Internal version)',
+        },
+      ],
+    },
+    {
+      label: 'Development',
+      items: [
+        {
+          label: 'APD API Reference',
+          docId: 'apdmaindoc',
+        },
+      ],
+    },
+    {
+      label: 'Configuration',
+      items: [
+        {
+          label: 'APD for Claims (Early Access)',
+          docId: 'apdclaimsinnsbruck',
+        },
+      ],
+    },
+  ],
+};
+
+export default function Apd() {
+  return <SectionLayout {...pageConfig} />;
+}
