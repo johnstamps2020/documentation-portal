a5abf91379868abc572f3184102fe132a3edc256
mskowron <mskowron@guidewire.com>
2023-10-19 18:40:57 +0200

    DOCS-3791 - Update run configurations for IntelliJ and scripts to use Docker volumes

--- server/start_docportal_db_container.sh
+++ server/start_docportal_db_container.sh
@@ -1,13 +1,14 @@
-echo "!!!!!!!!!!!!!!!!!!!"
-echo "If the DB does not work, try removing your local postgres files $(rm -rf /private/tmp/pgdata)"
-echo "!!!!!!!!!!!!!!!!!!!"
-
 container_name="docportal-db"
+db_volume_name="dev_db_volume"
+
+docker volume create --name $db_volume_name
+echo "The database volume is ready"
 
 docker start $container_name || docker run --name $container_name \
   -e POSTGRES_USER=postgres \
   -e POSTGRES_PASSWORD=testtesttest \
   -e PGDATA=/var/lib/postgresql/data/pgdata \
-  -v /tmp:/var/lib/postgresql/data \
+  -v dev_db_volume:/var/lib/postgresql/data \
   -p 5432:5432 \
   postgres:14.1-alpine
+echo "The database container is ready"
