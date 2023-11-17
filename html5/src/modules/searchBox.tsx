3380936dbadbb5fe78525543a5464173e5e09e4a
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-11-17 12:57:40 +0100

    upgrade to React 18

--- html5/src/modules/searchBox.tsx
+++ html5/src/modules/searchBox.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import '../stylesheets/modules/searchBox.css';
-import { render } from 'react-dom';
 import OfflineSearch from './components/Search/OfflineSearch';
 import OnlineSearch from './components/Search/OnlineSearch';
 
 export function addSearchBox(isOffline: boolean) {
   const searchContainer = document.getElementById('headerCenter');
+  const root = createRoot(searchContainer!);
 
   if (isOffline) {
-    render(<OfflineSearch />, searchContainer);
+    root.render(<OfflineSearch />);
   } else {
-    render(<OnlineSearch />, searchContainer);
+    root.render(<OnlineSearch />);
   }
 }
