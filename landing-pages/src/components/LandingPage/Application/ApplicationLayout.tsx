fb345a9043c2609b2c81d5b1e6ee80a3e8362791
Pawel Kowaluk <pkowaluk@guidewire.com>
2023-11-20 13:22:45 +0100

    change the styling assuming the login feature is below the button

--- landing-pages/src/components/LandingPage/Application/ApplicationLayout.tsx
+++ landing-pages/src/components/LandingPage/Application/ApplicationLayout.tsx
@@ -1,8 +1,6 @@
 import Stack from '@mui/material/Stack';
-import NotLoggedInInfo from 'components/NotLoggedInInfo';
 import ApplicationHero, { ApplicationHeroProps } from './ApplicationHero';
 import ApplicationTabs, { ApplicationTabItemProps } from './ApplicationTabs';
-import Container from '@mui/material/Container';
 
 export type ApplicationLayoutProps = ApplicationHeroProps & {
   tabs: ApplicationTabItemProps[];
@@ -16,9 +14,6 @@ export default function ApplicationLayout({
   return (
     <Stack gap="35px" sx={{ mb: 10 }}>
       <ApplicationHero buttonProps={buttonProps} title={title} />
-      <Container>
-        <NotLoggedInInfo />
-      </Container>
       <ApplicationTabs tabs={tabs} />
     </Stack>
   );
