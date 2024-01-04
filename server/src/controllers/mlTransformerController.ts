ee0fa4ab18d894c614b99584c7394f5c9ee1d2b4
mskowron <mskowron@guidewire.com>
2024-01-04 13:30:33 +0100

    DOCS-3653 - (WIP) Use the ML Transformer service to convert text to vectors

--- server/src/controllers/mlTransformerController.ts
+++ server/src/controllers/mlTransformerController.ts
@@ -1,37 +1,22 @@
-class FeatureExtractionPipeline {
-  static task = 'feature-extraction';
-  static model = 'multi-qa-MiniLM-L6-cos-v1';
-  static instance = null;
-
-  static async getInstance(progress_callback = null) {
-    if (this.instance === null) {
-      // Dynamically import the Transformers.js library
-      // By default, all import statements are converted to require during compilation.
-      //   the two lines below delay the import until after compilation.
-      //   https://stackoverflow.com/questions/76883048/err-require-esm-for-import-with-xenova-transformers
-      let TransformersApi = Function('return import("@xenova/transformers")')();
-      let { pipeline, env } = await TransformersApi;
-      env.allowRemoteModels = false;
-      env.localModelPath = process.env.MODEL_ABS_PATH;
-      // @ts-ignore
-      this.instance = pipeline(this.task, this.model, {
-        progress_callback,
-      });
-    }
-
-    return this.instance;
-  }
-}
-
 export async function createVectorsFromText(text: string) {
-  const extractor = await FeatureExtractionPipeline.getInstance();
-  if (extractor) {
-    // @ts-ignore
-    const tensor = await extractor(text, {
-      pooling: 'mean',
-      normalize: true,
+  try {
+    const response = await fetch('http://localhost:5555/encode', {
+      method: 'POST',
+      body: JSON.stringify({
+        text: text,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
     });
-    return tensor.flatten().tolist();
+
+    if (response.ok) {
+      return await response.json();
+    }
+    return [];
+  } catch (err) {
+    console.error(err);
+    return [];
   }
-  return null;
 }
