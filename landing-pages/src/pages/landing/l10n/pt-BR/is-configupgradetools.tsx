6076019879fddbd6cd485fb95ff866115c45fea0
Ryuichi Wakita <rwakita@guidewire.com>
2024-01-12 16:50:21 -0800

    update pt selector label for is-configupgradetools guide

--- landing-pages/src/pages/landing/l10n/pt-BR/is-configupgradetools.tsx
+++ landing-pages/src/pages/landing/l10n/pt-BR/is-configupgradetools.tsx
@@ -11,7 +11,7 @@ const pageConfig: CategoryLayoutProps = {
   },
   selector: {
     label: 'Selecione o produto',
-    selectedItemLabel: 'Cloud Platform',
+    selectedItemLabel: 'InsuranceSuite Configuration Upgrade Tools',
     items: allSelectors.s04aae4cce94a63b38358bb52ee3acace,
     labelColor: 'white',
   },
