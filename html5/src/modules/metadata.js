c68b49d89c5f8e50a2b04317061503ce87fd6fc2
Miles Gilster <mgilster@guidewire.com>
2024-01-19 11:45:37 -0800

    DOCS-5218 comment out console logs

--- html5/src/modules/metadata.js
+++ html5/src/modules/metadata.js
@@ -52,7 +52,7 @@ export async function setMetadata() {
   let sessionDocId = sessionStorage.getItem('docId');
 
   if (docId === sessionDocId) {
-    console.log('Fetching metadata from sessionStorage');
+    //console.log('Fetching metadata from sessionStorage');
     window.docProduct = sessionStorage.getItem('docProduct');
     window.docPlatform = sessionStorage.getItem('docPlatform');
     window.docVersion = sessionStorage.getItem('docVersion');
@@ -64,7 +64,7 @@ export async function setMetadata() {
     window.docInternal = sessionStorage.getItem('docInternal') === 'true';
     window.docEarlyAccess = sessionStorage.getItem('docEarlyAccess') === 'true';
   } else {
-    console.log('Fetching metadata from endpoint');
+    //console.log('Fetching metadata from endpoint');
     const response = await fetch(`/safeConfig/docMetadata/${docId}`);
     if (response.ok) {
       try {
