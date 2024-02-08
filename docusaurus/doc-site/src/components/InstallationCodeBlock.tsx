44810f880af52b567ef01dbbd26ab73adcba03b7
Pawel Kowaluk <pkowaluk@guidewire.com>
2024-02-08 16:04:11 +0100

    simplify doc display

--- docusaurus/doc-site/src/components/InstallationCodeBlock.tsx
+++ docusaurus/doc-site/src/components/InstallationCodeBlock.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import CodeBlock from '@theme/CodeBlock';
 import pluginInfo from '../../../plugins/gw-plugin-redoc/package.json';
 import themeInfo from '../../../themes/gw-theme-classic/package.json';
+import scriptsInfo from '../../../../scripts/package.json';
 
 type InstallationCodeBlockProps = {
-  forPlugin?: boolean;
+  packageName: string;
+};
+
+const versions = {
+  'gw-theme-classic': themeInfo.version,
+  'gw-plugin-redoc': pluginInfo.version,
 };
 
 export default function InstallationCodeBlock({
-  forPlugin,
+  packageName,
 }: InstallationCodeBlockProps) {
-  const installationString = forPlugin
-    ? `yarn add @doctools/gw-plugin-redoc@${pluginInfo.version}`
-    : `yarn add @doctools/gw-theme-classic@${themeInfo.version}`;
+  const installationString = `yarn add @doctools/${packageName}@${versions[packageName]}`;
 
   return <CodeBlock language="bash">{installationString}</CodeBlock>;
 }
