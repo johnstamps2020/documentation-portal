88539475ac7c428f304553e2595044f8c3986525
Pawel Kowaluk <pkowaluk@guidewire.com>
2024-03-19 14:45:40 +0100

    no style isolation

--- docusaurus/themes/gw-theme-classic/src/theme/OverlayLoader.tsx
+++ docusaurus/themes/gw-theme-classic/src/theme/OverlayLoader.tsx
@@ -1,16 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './OverlayLoader.css';
 
 export default function OverlayLoader() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    document.fonts.load('12px Source Sans Pro').then(() => setMounted(true));
-  }, []);
-
-  if (mounted) {
-    return null;
-  }
-
   return <div className="guidewire-overlay-loader" />;
 }
