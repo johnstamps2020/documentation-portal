a859e1ac2cc893f09f9a71fc033c5c369e27082a
Pawel Kowaluk <pkowaluk@guidewire.com>
2024-03-19 14:29:06 +0100

    more style isolation

--- docusaurus/themes/gw-theme-classic/src/theme/OverlayLoader.tsx
+++ docusaurus/themes/gw-theme-classic/src/theme/OverlayLoader.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './OverlayLoader.css';
 
 export default function OverlayLoader() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    document.fonts.load('12px Source Sans Pro').then(() => setMounted(true));
+  }, []);
+
+  if (mounted) {
+    return null;
+  }
+
   return <div className="guidewire-overlay-loader" />;
 }
--- docusaurus/themes/gw-theme-classic/src/theme/Root.tsx
+++ docusaurus/themes/gw-theme-classic/src/theme/Root.tsx
@@ -4,7 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import InitialRoot from '@theme-init/Root';
 import { DocContextProvider } from '@theme/DocContext';
 import InternalWrapper from '@theme/Internal/InternalWrapper';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import GwThemeProvider from './GwThemeProvider';
 import Init from './Init';
 import { NotificationProvider } from './NotificationContext/NotificationContext';
@@ -18,15 +18,9 @@ const faviconEncoded =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAgMAAAAOFJJnAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAACVBMVEUAAAAAdJ////9qrHgvAAAAAXRSTlMAQObYZgAAAAFiS0dEAmYLfGQAAAAHdElNRQflChQQMzjf2YYmAAAAJ0lEQVQY02NgIAmIhoIAsYwAmC58DFaYYmIYDAyMhBihoSFEMwgDAC5dG1EV41bhAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTEwLTIwVDE2OjUxOjU0KzAwOjAwtT96EwAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0xMC0yMFQxNjo1MTo1NCswMDowMMRiwq8AAAAASUVORK5CYII=';
 
 function Root({ children }: RootProps) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    document.fonts.load('12px Source Sans Pro').then(() => setMounted(true));
-  }, []);
-
   return (
     <GwThemeProvider>
-      {!mounted && <OverlayLoader />}
+      <OverlayLoader />
       <CssBaseline />
       <NotificationProvider>
         <InitialRoot>
