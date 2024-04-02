fb2b374c01b6f0f90277a68cd18f9f94b7a81a87
Miles Gilster <mgilster@guidewire.com>
2024-04-02 08:43:40 -0700

    DOCS-6017 add video description

--- landing-pages/src/pages/landing/cloudProducts/jasper/in.tsx
+++ landing-pages/src/pages/landing/cloudProducts/jasper/in.tsx
@@ -31,6 +31,9 @@ const pageConfig: ApplicationLayoutProps = {
           label: 'Release Video',
           url: 'https://youtu.be/ECG0VfS2VjU',
           videoIcon: true,
+          description: (
+            <>Watch a video that describes and demonstrates new features.</>
+          )
         },
         {
           label: 'Studio Release Notes',
