79803bb88338098ab42ac8b1b785c5aae4342e63
Pawel Kowaluk <pkowaluk@guidewire.com>
2024-04-10 15:29:56 +0200

    fix theme compatibility (DOCT-347)

--- docusaurus/themes/gw-theme-classic/src/index.ts
+++ docusaurus/themes/gw-theme-classic/src/index.ts
@@ -1,11 +1,15 @@
-import type { LoadContext, Plugin } from '@docusaurus/types';
 import type { PluginOptions } from '@docusaurus/theme-classic';
 import type { ThemeConfig } from '@docusaurus/theme-common';
-import { resolve } from 'path';
+import type {
+  LoadContext,
+  Plugin,
+  TranslationFileContent,
+} from '@docusaurus/types';
+import { readFileSync, readdirSync, statSync } from 'fs';
+import { extname, relative, resolve } from 'path';
 import { PluginData } from './types';
 import { PLUGIN_NAME } from './types/constants';
-import type { TranslationFileContent } from '@docusaurus/types';
-import { readFileSync } from 'fs';
+const matter = require('gray-matter');
 
 export function readJsonFile(fileName: string): { [id: string]: string } {
   const translationData: TranslationFileContent = JSON.parse(
@@ -36,10 +40,54 @@ const siteMetadata = {
   appBaseUrl: getAppBaseUrl(),
 };
 
+type PluginContent = {
+  internalDocIds: string[];
+};
+
+function getAllFilesRecursively(rootDir: string): string[] {
+  const filesAndFolders = readdirSync(rootDir);
+  return filesAndFolders
+    .map((fileOrFolder) => {
+      const absolutePath = resolve(rootDir, fileOrFolder);
+      if (statSync(absolutePath).isDirectory()) {
+        return getAllFilesRecursively(absolutePath);
+      }
+
+      return absolutePath;
+    })
+    .flat();
+}
+
+function getInternalDocIds(docsDir: string): PluginContent['internalDocIds'] {
+  const allDocFiles = getAllFilesRecursively(docsDir).filter((filePath) =>
+    ['.md', '.mdx'].includes(extname(filePath))
+  );
+
+  const internalDocIds = [];
+  for (const docFilePath of allDocFiles) {
+    const fileContents = readFileSync(docFilePath, 'utf-8');
+    const frontMatter = matter(fileContents).data;
+    if (!frontMatter) {
+      continue;
+    }
+
+    const isInternal = frontMatter.internal === true;
+    if (isInternal) {
+      const removeExtensionRegExp = new RegExp(`${extname(docFilePath)}$`);
+      const docId =
+        frontMatter.id ||
+        relative(docsDir, docFilePath).replace(removeExtensionRegExp, '');
+      internalDocIds.push(docId);
+    }
+  }
+
+  return internalDocIds;
+}
+
 export default async function (
   context: LoadContext,
   options: PluginOptions
-): Promise<Plugin<undefined>> {
+): Promise<Plugin<PluginContent>> {
   const {
     i18n: { currentLocale },
   } = context;
@@ -62,19 +110,16 @@ export default async function (
       return resolve(__dirname, '../src/theme');
     },
 
-    async contentLoaded({ actions, allContent }) {
-      const defaultContent: any =
-        allContent['docusaurus-plugin-content-docs'].default;
-      const topVersionContent = defaultContent.loadedVersions[0];
+    async loadContent() {
+      const internalDocIds = getInternalDocIds(
+        resolve(context.siteDir, 'docs')
+      );
+      return { internalDocIds };
+    },
 
-      // get internal docs
-      const docs = topVersionContent.docs;
-      const internalDocIds = docs
-        .map((d) => d.frontMatter.internal && d.unversionedId)
-        .filter(Boolean);
-
+    async contentLoaded({ actions, content }) {
       const pluginData: PluginData = {
-        internalDocIds,
+        internalDocIds: content.internalDocIds,
         ...siteMetadata,
       };
       const { setGlobalData } = actions;
