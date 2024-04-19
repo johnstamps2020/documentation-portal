fe79ba6c338e48c7a697516905016e5c3ac8a835
Pawel Kowaluk <pkowaluk@guidewire.com>
2024-04-19 09:23:28 +0200

    rename files and create placeholders (DOCT-340)

--- docusaurus/plugins/docusaurus-security/src/modules/postBuild.ts
+++ docusaurus/plugins/docusaurus-security/src/modules/postBuild.ts
@@ -1,65 +1,26 @@
-import { existsSync, readFileSync, rmSync } from 'fs';
-import jsdom from 'jsdom';
-import { extname, join, resolve } from 'path';
+import { existsSync, readFileSync, renameSync, rmSync } from 'fs';
+import { RenameRestoreItem, renameRestoreListFilePath } from './filterSidebars';
 import { isPublicBuild } from './helpers';
 
-const { JSDOM } = jsdom;
-
-function getHtmlFilePath(routePath: string): string | undefined {
-  if (!existsSync(routePath)) {
-    return undefined;
-  }
-
-  if (extname(routePath) === '.html') {
-    return routePath;
-  }
-
-  const indexHtmlPath = resolve(routePath, 'index.html');
-  if (existsSync(indexHtmlPath)) {
-    return indexHtmlPath;
-  }
-}
-
-export function deleteRestrictedFiles(
-  baseUrl: string,
-  routesPaths: string[],
-  outDir: string
-): void {
-  if (!isPublicBuild() || routesPaths.length === 0) {
+export function revertFileChanges(): void {
+  if (!isPublicBuild() || !existsSync(renameRestoreListFilePath)) {
     return;
   }
 
-  console.log('Deleting restricted files...');
-  routesPaths.forEach((route) => {
-    const routePath = join(outDir, route.replace(baseUrl, ''));
-    const filePath = getHtmlFilePath(routePath);
-    if (!filePath) {
-      console.error(`File not found for route ${routePath}`);
-      return;
+  const renameRestoreList = JSON.parse(
+    readFileSync(renameRestoreListFilePath, 'utf-8')
+  ) as RenameRestoreItem[];
+
+  for (const { oldPath, newPath } of renameRestoreList) {
+    if (!existsSync(newPath)) {
+      console.error('Cannot restore file name', newPath, 'file does not exist');
     }
 
-    const fileContents = readFileSync(filePath, 'utf8');
-    // read HTML file as HTML DOM
-    const htmlString = new JSDOM(fileContents);
-    const metaTag = htmlString.window.document.querySelector(
-      'meta[name="com.guidewire.metadata:public"]'
-    );
+    rmSync(oldPath);
+    renameSync(newPath, oldPath);
+    console.log('Renamed file back from', newPath, 'to', oldPath);
+  }
 
-    if (!metaTag) {
-      return;
-    }
-
-    if (metaTag.getAttribute('content') === 'true') {
-      return;
-    }
-
-    try {
-      console.log(`Deleting ${routePath}`);
-      rmSync(routePath, { recursive: true });
-    } catch (error) {
-      console.error(`Failed to delete ${routePath}`);
-      console.error('ABORTING BUILD');
-      process.exit(1);
-    }
-  });
+  console.log('Deleting the temporary rename list');
+  rmSync(renameRestoreListFilePath);
 }
