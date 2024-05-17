1f4ecabe07f2844b2a761a9329bbe8bb353b8c34
Pawel Kowaluk <pkowaluk@guidewire.com>
2024-05-17 15:16:42 +0200

    show the AI chat only on dev (DOCS-5278)

--- landing-pages/src/pages/landing/chat.tsx
+++ landing-pages/src/pages/landing/chat.tsx
@@ -41,7 +41,7 @@ export default function ChatPage() {
   }
 
   return (
-    <AccessControl allowedOnEnvs={['dev', 'staging']} accessLevel="everyone">
+    <AccessControl allowedOnEnvs={['dev']} accessLevel="everyone">
       <Container sx={{ height: mainHeight, overflowY: 'scroll' }}>
         <ChatProvider userInfo={userInfo}>
           <ChatWrapper />
