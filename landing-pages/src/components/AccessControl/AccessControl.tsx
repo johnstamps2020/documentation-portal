b05f5ed883efc0bbd8be5325edfdef22244a830a
Michal Skowron <mskowron@guidewire.com>
2024-07-10 09:55:51 -0700

    Applied suggestion

--- landing-pages/src/components/AccessControl/AccessControl.tsx
+++ landing-pages/src/components/AccessControl/AccessControl.tsx
@@ -121,7 +121,6 @@ export default function AccessControl({
   ]);
 
   if (userInfoLoading || userInfoError || envInfoLoading || envInfoError) {
-    return null;
 
   if (allowedToSee === false) {
     return null;
