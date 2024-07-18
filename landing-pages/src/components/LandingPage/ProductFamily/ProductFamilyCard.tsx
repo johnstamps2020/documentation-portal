79124ac82f399b15bc8b416e4df5e092cdd44c99
mskowron <mskowron@guidewire.com>
2024-07-18 11:49:13 +0200

    refactor: update Product Family Layout to use new hooks and contexts
    Task-id: DOCT-572

--- landing-pages/src/components/LandingPage/ProductFamily/ProductFamilyCard.tsx
+++ landing-pages/src/components/LandingPage/ProductFamily/ProductFamilyCard.tsx
@@ -1,19 +1,29 @@
 import Paper from '@mui/material/Paper';
-import { useLandingPageItems } from 'hooks/useLandingPageItems';
 import { LandingPageItemProps } from 'pages/LandingPage/LandingPageTypes';
 import LandingPageLink from 'components/LandingPage/LandingPageLink';
-import Skeleton from '@mui/material/Skeleton';
-import LandingPageItemRenderer from '../LandingPageItemRenderer';
-import { arrangeItems } from 'helpers/landingPageHelpers';
+import {
+  arrangeItems,
+  getListOfItemsToDisplayOnLandingPage,
+  LandingPageItemData,
+} from 'helpers/landingPageHelpers';
+import { useLandingPageItemsContext } from '../LandingPageItemsContext';
 
 export default function ProductFamilyCard(item: LandingPageItemProps) {
   const sx = {
     fontSize: 20,
     fontWeight: 800,
   };
-  const { landingPageItems, isLoading, isError } = useLandingPageItems([item]);
-  const arrangedLandingPageItems = arrangeItems([item], landingPageItems);
-  const productFamilyItem = (
+  const { allAvailableItems } = useLandingPageItemsContext();
+
+  if (!allAvailableItems) {
+    return null;
+  }
+
+  const itemsToDisplay: LandingPageItemData[] =
+    getListOfItemsToDisplayOnLandingPage([item], allAvailableItems);
+
+  const arrangedLandingPageItems = arrangeItems([item], itemsToDisplay);
+  return (
     <Paper
       sx={{
         height: '100px',
@@ -32,23 +42,5 @@ export default function ProductFamilyCard(item: LandingPageItemProps) {
         />
       )}
     </Paper>
-  );
-  const productFamilySkeleton = (
-    <Skeleton
-      variant="rectangular"
-      sx={{
-        width: { sm: '300px', xs: '100%' },
-        height: '100px',
-      }}
-    />
-  );
-  return (
-    <LandingPageItemRenderer
-      landingPageItems={arrangedLandingPageItems}
-      isLoading={isLoading}
-      isError={isError}
-      skeleton={productFamilySkeleton}
-      item={productFamilyItem}
-    />
   );
 }
