653f2a0ee81abec189fe849d0c4953230977a661
rwakita <rwakita@guidewire.com>
2024-09-11 15:20:20 -0700

    recover de-DE jasper books

--- landing-pages/src/pages/l10n/de-DE/bc.tsx
+++ landing-pages/src/pages/l10n/de-DE/bc.tsx
@@ -32,6 +32,19 @@ const pageConfig: CategoryLayoutProps = {
       ],
     },
     {
+      label: '2024.02 (jasper)',
+      items: [
+        {
+          label: 'Anwendungshandbuch',
+          docId: 'isbc202402deDEapp',
+        },
+        {
+          label: 'BillingCenter - Update-Handbuch',
+          docId: 'isbc202402deDEupdate',
+        },
+      ],
+    },
+    {
       label: '2023.10 (Innsbruck)',
       items: [
         {
