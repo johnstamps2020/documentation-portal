ce31d023607c0e4e102c55b9127b94c0d080dc0d
Pawel Kowaluk <pkowaluk@guidewire.com>
2024-09-17 14:52:03 +0200

    refactor: invoke okta strategies once (DOCT-739)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,68 +1,73 @@
 import 'dotenv/config';
 import http from 'http';
-import app from './app';
+import { getApp } from './app';
 import { winstonLogger } from './controllers/loggerController';
 
-function normalizePort(val: string) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-function getFormattedError(error: NodeJS.ErrnoException) {
-  const intro = 'Error in application runner (server.js):';
-  return `${intro}
-    CODE: ${error.code}
-    MESSAGE: ${error.message}
-    ERROR: ${JSON.stringify(error)}`;
-}
-
-function onError(error: NodeJS.ErrnoException) {
-  if (error.syscall !== 'listen') {
-    winstonLogger.error(getFormattedError(error));
-  }
-
-  var bind =
-    typeof httpServerPort === 'string'
-      ? 'Pipe ' + httpServerPort
-      : 'Port ' + httpServerPort;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      winstonLogger.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      winstonLogger.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      winstonLogger.error(getFormattedError(error));
-  }
-}
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
-  winstonLogger.notice('Listening on ' + bind);
-}
-
-const httpServerPort = normalizePort(process.env.PORT || '8081');
-
 // RUN APP IN SERVER
-const server = http.createServer(app);
-server.listen(httpServerPort);
+async function startServer() {
+  function normalizePort(val: string) {
+    const port = parseInt(val, 10);
 
-server.on('error', onError);
-server.on('listening', onListening);
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+
+    return false;
+  }
+
+  function getFormattedError(error: NodeJS.ErrnoException) {
+    const intro = 'Error in application runner (server.js):';
+    return `${intro}
+      CODE: ${error.code}
+      MESSAGE: ${error.message}
+      ERROR: ${JSON.stringify(error)}`;
+  }
+
+  function onError(error: NodeJS.ErrnoException) {
+    if (error.syscall !== 'listen') {
+      winstonLogger.error(getFormattedError(error));
+    }
+
+    var bind =
+      typeof httpServerPort === 'string'
+        ? 'Pipe ' + httpServerPort
+        : 'Port ' + httpServerPort;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        winstonLogger.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        winstonLogger.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        winstonLogger.error(getFormattedError(error));
+    }
+  }
+
+  function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
+    winstonLogger.notice('Listening on ' + bind);
+  }
+
+  const httpServerPort = normalizePort(process.env.PORT || '8081');
+
+  const app = await getApp();
+  const server = http.createServer(app);
+  server.listen(httpServerPort);
+
+  server.on('error', onError);
+  server.on('listening', onListening);
+}
+
+startServer();
