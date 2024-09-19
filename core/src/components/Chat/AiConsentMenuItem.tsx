3abf4f06c56cd2ddaaa9bd8a40482de1fb4062a1
Pawel Kowaluk <pkowaluk@guidewire.com>
2024-09-19 11:17:38 +0200

    feat: apply the legal layer (DOCT-772)

--- core/src/components/Chat/AiConsentMenuItem.tsx
+++ core/src/components/Chat/AiConsentMenuItem.tsx
@@ -1,12 +1,14 @@
 import FormControlLabel from '@mui/material/FormControlLabel';
+import MenuItem from '@mui/material/MenuItem';
 import Switch from '@mui/material/Switch';
-import React from 'react';
+import React, { useState } from 'react';
 import { translate } from '../../lib';
 import { useConsentStore } from '../../stores/consentStore';
 import { useEnvStore } from '../../stores/envStore';
-import MenuItem from '@mui/material/MenuItem';
+import { TermsAndConditionsDialogBox } from './TermsAndConditionsDialogBox';
 
 export function AiConsentMenuItem() {
+  const [open, setOpen] = useState(false);
   const aIConsented = useConsentStore((state) => state.aiConsented);
   const setAIConsented = useConsentStore((state) => state.setAIConsented);
   const envName = useEnvStore((state) => state.envName);
@@ -20,18 +22,25 @@ export function AiConsentMenuItem() {
     return null;
   }
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (aIConsented) {
+      setAIConsented(false);
+    } else {
+      setOpen(true);
+    }
+  };
+
   return (
     <MenuItem>
       <FormControlLabel
-        control={
-          <Switch
-            checked={aIConsented}
-            onChange={() => {
-              setAIConsented(!aIConsented);
-            }}
-          />
-        }
+        control={<Switch checked={aIConsented} onChange={handleChange} />}
         label={aiLabel}
+      />
+      <TermsAndConditionsDialogBox
+        open={open}
+        handleClose={() => {
+          setOpen(false);
+        }}
       />
     </MenuItem>
   );
--- core/src/components/Chat/TermsAndConditionsDialogBox.tsx
+++ core/src/components/Chat/TermsAndConditionsDialogBox.tsx
@@ -1,10 +1,11 @@
+import Button from '@mui/material/Button';
 import Dialog, { DialogProps } from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import React from 'react';
 import { Translate } from '../../lib';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
+import { useConsentStore } from '../../stores/consentStore';
 
 type TermsAndConditionsDialogBoxProps = DialogProps & {
   handleClose: () => void;
@@ -14,6 +15,8 @@ export function TermsAndConditionsDialogBox({
   open,
   handleClose,
 }: TermsAndConditionsDialogBoxProps) {
+  const setAIConsented = useConsentStore((state) => state.setAIConsented);
+
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>
@@ -21,14 +24,18 @@ export function TermsAndConditionsDialogBox({
           title goes here
         </Translate>
       </DialogTitle>
-      <DialogContent>Coming soon!</DialogContent>
+      <DialogContent>
+        <Translate id="chat.termsAndConditions.body">body goes here</Translate>
+      </DialogContent>
       <DialogActions>
         <Button
           onClick={() => {
+            setAIConsented(true);
             handleClose();
           }}
+          variant="contained"
         >
-          <Translate id="chat.termsAndConditions.close">Close</Translate>
+          <Translate id="chat.termsAndConditions.close">Got it</Translate>
         </Button>
       </DialogActions>
     </Dialog>
